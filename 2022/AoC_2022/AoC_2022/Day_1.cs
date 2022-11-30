2a5b6e825366a2d80086ebeec89b353643378fce
Renzo Baasdam <r.baasdam@tjip.com>
2022-11-30 15:25:26 +0100

    Add 2022

---
+++ 2022/AoC_2022/AoC_2022/Day_1.cs
@@ -0,0 +1,16 @@
+namespace AoC_2022;
+
+public class Day_1
+{
+    [TestCase(2, ExpectedResult = 1)]
+    public int Part_1(int i)
+    {
+        return i;
+    }
+
+    [TestCase(3, ExpectedResult = 2)]
+    public int Part_2(int i)
+    {
+        return i;
+    }
+}
