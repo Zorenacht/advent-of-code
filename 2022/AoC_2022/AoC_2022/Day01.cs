6bf9ddc98fecab5d4efce837a45109cf82b4f6c7
Renzo Baasdam <r.baasdam@tjip.com>
2022-12-01 09:59:01 +0100

    Day 1

---
+++ 2022/AoC_2022/AoC_2022/Day01.cs
@@ -0,0 +1,59 @@
+namespace AoC_2022;
+
+public class Day01
+{
+    readonly string[] InputPart_1 = Reader.ReadLines(@"InputFiles\Day01-1.txt").ToArray();
+    readonly string[] InputPart_2 = Reader.ReadLines(@"InputFiles\Day01-2.txt").ToArray();
+
+    [Test]
+    public void Part1()
+    {
+        int result = 0;
+        int current = 0;
+        foreach (string line in InputPart_1)
+        {
+            if(line == string.Empty)
+            {
+                if(current > result)
+                {
+                    result = current;
+                }
+                current = 0;
+            }
+            else
+            {
+                current += int.Parse(line);
+            }
+        }
+        result.Should().Be(68923);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        int current = 0;
+        var top3 = new PriorityQueue<int, int>();
+        List<int> input = new List<int>();
+        foreach (string line in InputPart_2)
+        {
+            if (line == string.Empty)
+            {
+                bool notEmpty = top3.TryPeek(out int _, out int prio);
+                if ( !notEmpty || notEmpty && prio < current)
+                {
+                    if(top3.Count == 3)
+                    { 
+                        top3.Dequeue();
+                    }
+                    top3.Enqueue(current, current);
+                }
+                current = 0;
+            }
+            else
+            {
+                current += int.Parse(line);
+            }
+        }
+        (top3.Dequeue() + top3.Dequeue() + top3.Dequeue()).Should().Be(200044);
+    }
+}
--- 2022/AoC_2022/AoC_2022/Day02.cs
+++ 2022/AoC_2022/AoC_2022/Day02.cs
@@ -1,15 +1,15 @@
 namespace AoC_2022;
 
-public class Day_02
+public class Day02
 {
     [TestCase(2, ExpectedResult = 1)]
-    public int Part_1(int i)
+    public int Part1(int i)
     {
         return i;
     }
 
     [TestCase(3, ExpectedResult = 2)]
-    public int Part_2(int i)
+    public int Part2(int i)
     {
         return i;
     }
