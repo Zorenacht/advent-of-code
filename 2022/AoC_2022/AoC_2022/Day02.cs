3c06bb1c2d8e19bb42fb2e975eb26ef2c8dafa5a
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-02 07:00:42 +0100

    Initial Day 2

--- 2022/AoC_2022/AoC_2022/Day02.cs
+++ 2022/AoC_2022/AoC_2022/Day02.cs
@@ -5,34 +5,79 @@ public class Day02 : Day
     [Test]
     public void Example()
     {
-        int result = 0;
+        int score = 0;
         foreach (string line in InputExample)
         {
-            var split = line.Split(',', '.', ':');
+            var split = line.Split(' ');
+            char opponent = split[0][0];
+            char you = (char)(split[1][0] - ('X' - 'A'));
+            score += you - 'A' + 1;
+            if (opponent == you)
+            {
+                score += 3;
+            }
+            else if (opponent == 'A' && you == 'B' || opponent == 'B' && you == 'C' || opponent == 'C' && you == 'A')
+            {
+                score += 6;
+            }
         }
-        result.Should().Be(68923);
-        Assert.Pass();
+        score.Should().Be(15);
     }
 
     [Test]
     public void Part1()
     {
-        int result = 0;
+        int score = 0;
         foreach (string line in InputPart1)
         {
+            var split = line.Split(' ');
+            char opponent = split[0][0];
+            char you = (char)(split[1][0] - ('X' - 'A'));
+            score += you - 'A' + 1;
+            if (opponent == you)
+            {
+                score += 3;
+            }
+            else if (opponent == 'A' && you == 'B' || opponent == 'B' && you == 'C' || opponent == 'C' && you == 'A')
+            {
+                score += 6;
+            }
         }
-        result.Should().Be(68923);
-        Assert.Pass();
+        score.Should().Be(9241);
     }
 
     [Test]
     public void Part2()
     {
-        int result = 0;
+        int score = 0;
         foreach (string line in InputPart2)
         {
+            var split = line.Split(' ');
+            char opponent = split[0][0];
+            char you = split[1][0];
+            var offset = (you - 'Y') % 3; //+0 for draw, +1 for win, +2 for loss
+            score += Modulo(opponent - 'A' + offset,3) + 1;
+            score += 3 * (you - 'X');
         }
-        result.Should().Be(68923);
-        Assert.Pass();
+        score.Should().Be(14610);
+    }
+
+    [Test]
+    public void Modulo()
+    {
+        Modulo(-4, 3).Should().Be(2);
+        Modulo(-3, 3).Should().Be(0);
+        Modulo(-2, 3).Should().Be(1);
+        Modulo(-1, 3).Should().Be(2);
+        Modulo(0, 3).Should().Be(0);
+        Modulo(1, 3).Should().Be(1);
+        Modulo(2, 3).Should().Be(2);
+        Modulo(3, 3).Should().Be(0);
+    }
+
+    private int Modulo(int a, int b)
+    {
+        int remainder = a % b;
+        return remainder >= 0 ? remainder : remainder + b;
     }
 }
