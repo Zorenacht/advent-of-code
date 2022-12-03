20522f4ee783043eae9e67572b9d1e28fbd8367d
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-03 10:54:04 +0100

    Day 03 Initial

--- 2022/AoC_2022/AoC_2022/Day03.cs
+++ 2022/AoC_2022/AoC_2022/Day03.cs
@@ -1,3 +1,6 @@
+using MathNet.Numerics.Providers.LinearAlgebra;
+using System.Runtime.Serialization;
+
 namespace AoC_2022;
 
 public class Day03 : Day
@@ -6,29 +9,103 @@ public class Day03 : Day
     public void Example()
     {
         int score = 0;
+        var seen = new HashSet<char>(52);
         foreach (string line in InputExample)
         {
+            for(int i=0; i < line.Length/2; i++)
+            {
+                seen.Add(line[i]);
+            }
+            for (int i = line.Length / 2; i < line.Length; i++)
+            {
+                if (seen.Contains(line[i]))
+                {
+                    if (line[i] >= 'a' && line[i] <= 'z')
+                    {
+                        score += line[i] - 'a' + 1;
+                        break;
+                    }
+                    else
+                    {
+                        score += line[i] - 'A' + 1 + 26;
+                        break;
+                    }
+                };
+            }
+            seen.Clear();
         }
-        score.Should().Be(9241);
+        score.Should().Be(157);
     }
 
     [Test]
     public void Part1()
     {
         int score = 0;
+        var seen = new HashSet<char>(52);
         foreach (string line in InputPart1)
         {
+            for (int i = 0; i < line.Length / 2; i++)
+            {
+                seen.Add(line[i]);
+            }
+            for (int i = line.Length / 2; i < line.Length; i++)
+            {
+                if (seen.Contains(line[i]))
+                {
+                    if (line[i] >= 'a' && line[i] <= 'z')
+                    {
+                        score += line[i] - 'a' + 1;
+                        break;
+                    }
+                    else
+                    {
+                        score += line[i] - 'A' + 1 + 26;
+                        break;
+                    }
+                };
+            }
+            seen.Clear();
         }
-        score.Should().Be(9241);
+        score.Should().Be(7903);
     }
 
     [Test]
     public void Part2()
     {
         int score = 0;
+        var seen = new int[52];
+        int groupCounter = 0;
         foreach (string line in InputPart2)
         {
+            if(groupCounter == 3)
+            {
+                seen = new int[52];
+                groupCounter = 0;
+            }
+            foreach (var uniqueChar in line.Distinct())
+            {
+                int index;
+                if (uniqueChar >= 'a' && uniqueChar <= 'z')
+                {
+                    index = uniqueChar - 'a';
+                }
+                else
+                {
+                    index = uniqueChar - 'A' + 26;
+                }
+
+                if (seen[index] == 2)
+                {
+                    score += index + 1;
+                    break;
+                }
+                else
+                {
+                    seen[index] += 1;
+                }
+            }
+            groupCounter++;
         }
-        score.Should().Be(14610);
+        score.Should().Be(7903);
     }
 }
