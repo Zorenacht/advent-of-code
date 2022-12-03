af42a1020982607fd7197312a73bd8adc357d775
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-03 11:02:39 +0100

    Setup day 4

---
+++ 2022/AoC_2022/AoC_2022/Day04.cs
@@ -0,0 +1,34 @@
+namespace AoC_2022;
+
+public class Day04 : Day
+{
+    [Test]
+    public void Example()
+    {
+        int score = 0;
+        foreach (string line in InputExample)
+        {
+        }
+        score.Should().Be(157);
+    }
+
+    [Test]
+    public void Part1()
+    {
+        int score = 0;
+        foreach (string line in InputPart1)
+        {
+        }
+        score.Should().Be(7903);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        int score = 0;
+        foreach (string line in InputPart2)
+        {
+        }
+        score.Should().Be(2548);
+    }
+}
