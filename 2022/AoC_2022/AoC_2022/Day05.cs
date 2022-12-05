9003bac0de834c2bfa1bcc3f5aa4d58eb1c13799
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-05 18:56:48 +0100

    Cleanup day 5

--- 2022/AoC_2022/AoC_2022/Day05.cs
+++ 2022/AoC_2022/AoC_2022/Day05.cs
@@ -1,40 +1,17 @@
+using System.ComponentModel.Design;
 using System.Text.RegularExpressions;
 
 namespace AoC_2022;
 
-public sealed class Day05 : Day
+public sealed partial class Day05 : Day
 {
     [Test]
     public void Example()
     {
         const int NoContainers = 3;
         const int InitialHeight = 3;
-
-        Stack<char>[] containers = Enumerable.Range(1, NoContainers).Select(i => new Stack<char>()).ToArray();
-
-        for (int j = 0; j < NoContainers; j++)
-        {
-            for (int i = InitialHeight - 1; i >= 0; i--)
-            {
-                var container = InputExample[i][1 + 4 * j];
-                if (container != ' ')
-                {
-                    containers[j].Push(container);
-                }
-            }
-        }
-        foreach (string line in InputExample.Skip(InitialHeight+2))
-        {
-            var order = Regex.Match(line, @"move (\d+) from (\d+) to (\d+)").Groups;
-            int amount = int.Parse(order[1].Value);
-            int from = int.Parse(order[2].Value) - 1;
-            int to = int.Parse(order[3].Value) - 1;
-
-            for (int i = 0; i < amount; i++)
-            {
-                containers[to].Push(containers[from].Pop());
-            }
-        }
+        Parse(InputExample, InitialHeight, NoContainers, out var containers);
+        Commands(InputExample, InitialHeight, containers, CrateMover);
         string result = new(containers.Select(c => c.Peek()).ToArray());
         result.Should().Be("CMZ");
     }
@@ -44,32 +21,8 @@ public sealed class Day05 : Day
     {
         const int NoContainers = 9;
         const int InitialHeight = 8;
-
-        Stack<char>[] containers = Enumerable.Range(1, NoContainers).Select(i => new Stack<char>()).ToArray();
-
-        for (int j = 0; j < NoContainers; j++)
-        {
-            for (int i = InitialHeight-1; i >= 0; i--)
-            {
-                var container = InputPart1[i][1 + 4 * j];
-                if (container != ' ')
-                {
-                    containers[j].Push(container);
-                }
-            }
-        }
-        foreach (string line in InputPart1.Skip(InitialHeight+2))
-        {
-            var order = Regex.Match(line, @"move (\d+) from (\d+) to (\d+)").Groups;
-            int amount = int.Parse(order[1].Value);
-            int from = int.Parse(order[2].Value) - 1;
-            int to = int.Parse(order[3].Value) - 1;
-
-            for (int i = 0; i < amount; i++)
-            {
-                containers[to].Push(containers[from].Pop());
-            }
-        }
+        Parse(InputPart1, InitialHeight, NoContainers, out var containers);
+        Commands(InputPart1, InitialHeight, containers, CrateMover);
         string result = new(containers.Select(c => c.Peek()).ToArray());
         result.Should().Be("ZRLJGSCTR");
     }
@@ -79,38 +32,62 @@ public sealed class Day05 : Day
     {
         const int NoContainers = 9;
         const int InitialHeight = 8;
+        Parse(InputPart2, InitialHeight, NoContainers, out var containers);
+        Commands(InputPart2, InitialHeight, containers, CrateMover9001);
+        string result = new(containers.Select(c => c.Peek()).ToArray());
+        result.Should().Be("PRTTGRFPB");
+    }
 
-        Stack<char>[] containers = Enumerable.Range(1, NoContainers).Select(i => new Stack<char>()).ToArray();
-
-        for (int j = 0; j < NoContainers; j++)
+    private void Parse(string[] input, int height, int width, out Stack<char>[] containers)
+    {
+        containers = Enumerable.Range(1, width).Select(i => new Stack<char>()).ToArray();
+        for (int j = 0; j < width; j++)
         {
-            for (int i = InitialHeight - 1; i >= 0; i--)
+            for (int i = height - 1; i >= 0; i--)
             {
-                var container = InputPart2[i][1 + 4 * j];
+                var container = input[i][1 + 4 * j];
                 if (container != ' ')
                 {
                     containers[j].Push(container);
                 }
             }
         }
-        foreach (string line in InputPart2.Skip(InitialHeight + 2))
+    }
+
+    [GeneratedRegex("move (\\d+) from (\\d+) to (\\d+)")]
+    private static partial Regex CommandRegex();
+
+    private void Commands(string[] input, int height, Stack<char>[] containers, Action<int, int, int, Stack<char>[]> crateMover)
+    {
+        foreach (string line in input.Skip(height + 2))
         {
-            var order = Regex.Match(line, @"move (\d+) from (\d+) to (\d+)").Groups;
+            var order = CommandRegex().Match(line).Groups;
             int amount = int.Parse(order[1].Value);
             int from = int.Parse(order[2].Value) - 1;
             int to = int.Parse(order[3].Value) - 1;
-
-            var temp = new Stack<char>();
-            for (int i = 0; i < amount; i++)
-            {
-                temp.Push(containers[from].Pop());
-            }
-            for (int i = 0; i < amount; i++)
-            {
-                containers[to].Push(temp.Pop());
-            }
+            crateMover(amount, from, to, containers);
         }
-        string result = new(containers.Select(c => c.Peek()).ToArray());
-        result.Should().Be("PRTTGRFPB");
     }
+
+    private void CrateMover(int amount, int from, int to, Stack<char>[] containers)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            containers[to].Push(containers[from].Pop());
+        }
+    }
+
+    private void CrateMover9001(int amount, int from, int to, Stack<char>[] containers)
+    {
+        var temp = new Stack<char>();
+        for (int i = 0; i < amount; i++)
+        {
+            temp.Push(containers[from].Pop());
+        }
+        for (int i = 0; i < amount; i++)
+        {
+            containers[to].Push(temp.Pop());
+        }
+    }
+
 }
