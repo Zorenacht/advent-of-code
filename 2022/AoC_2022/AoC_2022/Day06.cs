89b661c617ce50e801d21825c1c6c68c5354c711
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-05 19:01:44 +0100

    Setup day 6

---
+++ 2022/AoC_2022/AoC_2022/Day06.cs
@@ -0,0 +1,35 @@
+namespace AoC_2022;
+
+public sealed partial class Day06 : Day
+{
+    [Test]
+    public void Example()
+    {
+        int result = 0;
+        foreach (string line in InputExample)
+        {
+        }
+        result.Should().Be(68923);
+    }
+
+    [Test]
+    public void Part1()
+    {
+        int result = 0;
+        foreach (string line in InputPart1)
+        {
+        }
+        result.Should().Be(68923);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        int result = 0;
+        foreach (string line in InputPart2)
+        {
+        }
+        result.Should().Be(68923);
+    }
+
+}
