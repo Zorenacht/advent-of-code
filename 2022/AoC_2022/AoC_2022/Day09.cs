b938e2dfb33af91c10e74b331134963faa017701
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-08 21:37:29 +0100

    Day 09 initial

---
+++ 2022/AoC_2022/AoC_2022/Day09.cs
@@ -0,0 +1,20 @@
+namespace AoC_2022;
+
+public sealed partial class Day09 : Day
+{
+    [Test]
+    public void Example()
+    {
+    }
+
+    [Test]
+    public void Part1()
+    {
+    }
+
+    [Test]
+    public void Part2()
+    {
+    }
+
+}
