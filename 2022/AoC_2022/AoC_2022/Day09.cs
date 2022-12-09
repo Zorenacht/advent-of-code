2ac53b4322f0932f7c9fcc3165353108888711c7
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-09 10:16:59 +0100

    Day 9 cleanup

--- 2022/AoC_2022/AoC_2022/Day09.cs
+++ 2022/AoC_2022/AoC_2022/Day09.cs
@@ -9,39 +9,22 @@ public sealed partial class Day09 : Day
     [Test]
     public void Example()
     {
-        var visited = new List<Point>();
-        var head = new Point(49, 49);
-        var tail = new Point(49, 49);
-        visited.Add(tail);
+        var rope = Enumerable.Repeat(new Point(0, 0), 2).ToArray();
+        var visited = new HashSet<Point>() { rope[^1] };
         foreach (string line in InputExample)
         {
             var split = line.Split(' ');
-            var dir = split[0];
+            var dir = StringToDirection(split[0]);
             var amount = int.Parse(split[1]);
-            var direction = dir switch
-            {
-                "R" => Direction.E,
-                "L" => Direction.W,
-                "U" => Direction.N,
-                "D" => Direction.S,
-                _ => throw new IndexOutOfRangeException(),
-            };
             for (int i = 0; i < amount; i++)
             {
-                head = head.Neighbor(direction);
-                var diff = head.Difference(tail);
-                if (diff.Norm > 2)
+                rope[0] = rope[0].Neighbor(dir);
+                UpdateTail(rope);
+                if (!visited.Contains(rope[^1]))
                 {
-                    tail = tail.Neighbor(diff.GeneralDirection);
-                    if (!visited.Contains(tail))
-                    {
-                        visited.Add(tail);
-                    }
+                    visited.Add(rope[^1]);
                 }
-            }/*
-            Console.WriteLine("---------");
-            Console.WriteLine(head);
-            Console.WriteLine(tail);*/
+            }
         }
         visited.Count().Should().Be(13);
     }
@@ -49,39 +32,22 @@ public sealed partial class Day09 : Day
     [Test]
     public void Part1()
     {
-        var visited = new List<Point>();
-        var head = new Point(49, 49);
-        var tail = new Point(49, 49);
-        visited.Add(tail);
+        var rope = Enumerable.Repeat(new Point(0, 0), 2).ToArray();
+        var visited = new HashSet<Point>() { rope[^1] };
         foreach (string line in InputPart1)
         {
             var split = line.Split(' ');
-            var dir = split[0];
+            var dir = StringToDirection(split[0]);
             var amount = int.Parse(split[1]);
-            var direction = dir switch
-            {
-                "R" => Direction.E,
-                "L" => Direction.W,
-                "U" => Direction.N,
-                "D" => Direction.S,
-                _ => throw new IndexOutOfRangeException(),
-            };
             for (int i = 0; i < amount; i++)
             {
-                head = head.Neighbor(direction);
-                var diff = head.Difference(tail);
-                if (diff.Norm > 2)
+                rope[0] = rope[0].Neighbor(dir);
+                UpdateTail(rope);
+                if (!visited.Contains(rope[^1]))
                 {
-                    tail = tail.Neighbor(diff.GeneralDirection);
-                    if (!visited.Contains(tail))
-                    {
-                        visited.Add(tail);
-                    }
+                    visited.Add(rope[^1]);
                 }
-            }/*
-            Console.WriteLine("---------");
-            Console.WriteLine(head);
-            Console.WriteLine(tail);*/
+            }
         }
         visited.Count().Should().Be(6044);
     }
@@ -90,37 +56,34 @@ public sealed partial class Day09 : Day
     public void Part2()
     {
 
-        var visited = new List<Point>();
-        var rope = Enumerable.Repeat(new Point(0,0), 10).ToArray();
-        visited.Add(rope[^1]);
+        var rope = Enumerable.Repeat(new Point(0, 0), 10).ToArray();
+        var visited = new HashSet<Point>() { rope[^1] };
         foreach (string line in InputPart1)
         {
             var split = line.Split(' ');
-            var dir = split[0];
+            var dir = StringToDirection(split[0]);
             var amount = int.Parse(split[1]);
-            var direction = dir switch
-            {
-                "R" => Direction.E,
-                "L" => Direction.W,
-                "U" => Direction.N,
-                "D" => Direction.S,
-                _ => throw new IndexOutOfRangeException(),
-            };
             for (int i = 0; i < amount; i++)
             {
-                rope[0] = rope[0].Neighbor(direction);
+                rope[0] = rope[0].Neighbor(dir);
                 UpdateTail(rope);
                 if (!visited.Contains(rope[^1]))
                 {
                     visited.Add(rope[^1]);
                 }
             }
-            Console.WriteLine("---------");
-            Console.WriteLine(rope[0]);
-            Console.WriteLine(rope[^1]);
         }
         visited.Count().Should().Be(2384);
     }
+
+    private Direction StringToDirection(string s) => s switch
+    {
+        "R" => Direction.E,
+        "L" => Direction.W,
+        "U" => Direction.N,
+        "D" => Direction.S,
+        _ => throw new IndexOutOfRangeException(),
+    };
 
     private void UpdateTail(Point[] rope)
     {
