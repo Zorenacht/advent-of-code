ee0c7f4f36b7b8193e65d21f1e79d2ea4aa32c30
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-11 22:27:10 +0100

    Setup day 12

---
+++ 2022/AoC_2022/AoC_2022/Day12.cs
@@ -0,0 +1,23 @@
+namespace AoC_2022;
+
+public sealed partial class Day12 : Day
+{
+    [Test]
+    public void Example() => Simulate(InputExample).Should().Be(-1);
+    [Test]
+    public void Part1() => Simulate(InputPart1).Should().Be(-1);
+    [Test]
+    public void ExampleP2() => Simulate(InputExample).Should().Be(-1);
+    [Test]
+    public void Part2() => Simulate(InputPart1).Should().Be(-1);
+
+
+    private long Simulate(string[] lines)
+    {
+        foreach(var line in lines)
+        {
+
+        }
+        return -1;
+    }
+}
