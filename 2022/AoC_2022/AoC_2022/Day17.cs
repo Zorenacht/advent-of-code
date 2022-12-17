cc5a75261784b5e3b341b69ef053461254e3f5a6
Renzo Baasdam <renzobaasdam@gmail.com>
2022-12-17 20:34:24 +0100

    Cleanup

--- 2022/AoC_2022/AoC_2022/Day17.cs
+++ 2022/AoC_2022/AoC_2022/Day17.cs
@@ -1,35 +1,37 @@
 using Tools.Geometry;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace AoC_2022;
 
 
 public sealed partial class Day17 : Day
 {
     [Test]
-    public void Example() => Tetris.Parse(InputExample).Simulate2(2022).Should().Be(3068);
+    public void Example() => Tetris.Parse(InputExample).BruteForceHeight(2022).Should().Be(3068);
     [Test]
-    public void Part1() => Tetris.Parse(InputPart1).Simulate2(2022).Should().Be(3130);
+    public void Part1() => Tetris.Parse(InputPart1).BruteForceHeight(2022).Should().Be(3130);
 
     [Test]
-    public void ExampleP2() => TwoTetrices.Parse(InputExample).FindHeight(1000000000000L).Should().Be(1514285714288L);
+    public void ExampleP2() => TwoTetrices.Parse(InputExample).FloydCycleHeight(1000000000000L).Should().Be(1514285714288L);
     [Test]
-    public void Part2() => TwoTetrices.Parse(InputPart1).FindHeight(1000000000000L).Should().Be(1);
+    public void Part2() => TwoTetrices.Parse(InputPart1).FloydCycleHeight(1000000000000L).Should().Be(1556521739139L);
 
     private class TwoTetrices
     {
-        public Tetris Me { get; set; }
-        public Tetris Elephant { get; set; }
-        public Tetris MeReset { get; set; }
+        private readonly Tetris Me;
+        private readonly Tetris Elephant;
+        private readonly Tetris MeReset;
 
-        private Dictionary<long, long> Dict = new Dictionary<long, long>();
-
-        public TwoTetrices(string[] input)
+        private TwoTetrices(string[] input)
         {
             Me = Tetris.Parse(input);
             Elephant = Tetris.Parse(input);
             MeReset = Tetris.Parse(input);
         }
 
-        public long FindHeight(long rocks)
+        public static TwoTetrices Parse(string[] input) => new TwoTetrices(input);
+
+        public long FloydCycleHeight(long rocks)
         {
             //Find cycle
             do
@@ -63,91 +65,54 @@ public sealed partial class Day17 : Day
                 MeReset.AddBlock();
             }
             var remainderHeight = MeReset.Height - periodHeight - initialHeight;
-            var a = rocks / period;
             return initialHeight + remainderHeight + (rocks - initial) / period * periodHeight;
         }
-
-        public static TwoTetrices Parse(string[] input) => new TwoTetrices(input);
     }
-
 
     private class Tetris
     {
+        public TetrisState CurrentState => new TetrisState(Rocks, BlockCount, BlockType, WindIndex);
+        public long BlockCount { get; set; }
+        public long Height => RockHeight + Rocks.ToList().FindLastIndex(x => x != 0);
+
+
         private readonly string Wind;
-        private int BlockType { get; set; }
-
         private int WindIndex { get; set; }
+        private int BlockType { get; set; }
+        private TetrisBlock? CurrentBlock { get; set; }
+        private long RockHeight { get; set; }
+        private int[] Rocks { get; set; }
 
-        private static readonly int MaxBlockCount = 2022;
+        public static Tetris Parse(string[] lines) => new Tetris(lines[0]);
 
-        public Tetris(string wind)
+        private Tetris(string wind)
         {
             Wind = wind;
-
             Rocks = Enumerable.Repeat(0, 80).ToArray();
             Rocks[0] = (1 << 30) - 1;
         }
 
-        public long Simulate2(int times)
+        public long BruteForceHeight(int times)
         {
             for (int i = 0; i < times; i++)
             {
                 AddBlock();
             }
-            Console.WriteLine(string.Join("\n", Rocks.Skip(0).Take(3000).Where(x => x != 0).Reverse().Select(x => Convert.ToString(x, 2).PadLeft(7, '0'))));
             return Height;
         }
 
-
-        public class State
+        public void AddBlock()
         {
-            int[] Rocks;
-            public long BlockCount { get; }
-            int BlockType;
-            int WindIndex;
-
-            public State(int[] rocks, long blockCount, int blockType, int windIndex)
-            {
-                Rocks = rocks;
-                BlockCount = blockCount;
-                BlockType = blockType;
-                WindIndex = windIndex;
-            }
-
-            public static bool operator ==(State self, State other)
-            {
-                return self?.BlockType == other?.BlockType
-                && self?.WindIndex == other?.WindIndex
-                && Enumerable.SequenceEqual(self.Rocks, other.Rocks);
-            }
-
-            public static bool operator !=(State self, State other) => !(self == other);
-        };
-
-        public State CurrentState => new State(Rocks, BlockCount, BlockType, WindIndex);
-
-        public Block CurrentBlock { get; set; }
-        private long RockHeight { get; set; }
-        public int[] Rocks { get; set; }
-        public long BlockCount { get; set; }
-        public long Height => RockHeight + Rocks.ToList().FindLastIndex(x => x != 0);
-
-        private int FullRow = (1 << 7) - 1;
-
-        public long AddBlock()
-        {
-            CurrentBlock = new Block(BlockType);
+            CurrentBlock = new TetrisBlock(BlockType);
             var blockHeight = Rocks.ToList().FindLastIndex(x => x != 0) + 4;
             while (true)
             {
                 var windIndex = WindIndex;
                 var wind = Wind[WindIndex];
+
                 //try to be blown by wind
                 if (wind == '>') CurrentBlock.MoveRight();
                 else if (wind == '<') CurrentBlock.MoveLeft();
-                else throw new Exception();
-
-                //Console.WriteLine(string.Join("\n", rocks.Take(20).Reverse().Select(x => Convert.ToString(x, 2).PadLeft(7, '0'))));
 
                 //if result overlaps move back
                 if (CurrentBlock.Overlap(Rocks[blockHeight..(blockHeight + 4)]))
@@ -155,8 +120,6 @@ public sealed partial class Day17 : Day
                     if (wind == '>') CurrentBlock.MoveLeft();
                     else if (wind == '<') CurrentBlock.MoveRight();
                 }
-                //Console.WriteLine("After moving to the side");
-                //Console.WriteLine(string.Join("\n", block.Occupied.Reverse().Select(x => Convert.ToString(x, 2).PadLeft(7, '0'))));
 
                 //try move block down
                 blockHeight--;
@@ -168,31 +131,23 @@ public sealed partial class Day17 : Day
                     for (int i = 0; i < 4; i++)
                     {
                         Rocks[blockHeight + i] = Rocks[blockHeight + i] | CurrentBlock.Occupied[i];
-                        if (Rocks[blockHeight + i] == FullRow) min = blockHeight + i;
                     }
 
-                    //Console.WriteLine("Before remove");
-                    //Console.WriteLine(string.Join("\n", rocks.Take(20).Reverse().Select(x => Convert.ToString(x, 2).PadLeft(7, '0'))));
-                    //var min = Rocks.ToList().FindLastIndex(x => x == (1 << 7) - 1);
+                    //Determine obsolete rocks
                     min = CleanupRocks();
-
                     if (min > 0)
                     {
                         RockHeight += min;
                         Rocks = Rocks.Skip(min).Concat(Enumerable.Repeat(0, min)).ToArray();
                     }
-                    //Console.WriteLine("After remove");
-                    //Console.WriteLine(string.Join("\n", rocks.Take(20).Reverse().Select(x => Convert.ToString(x, 2).PadLeft(7, '0'))));
 
                     BlockCount++;
                     WindIndex = (WindIndex + 1) % Wind.Length;
-
                     break;
                 }
                 WindIndex = (WindIndex + 1) % Wind.Length;
             }
             BlockType = (BlockType + 1) % 5;
-            return 0;
         }
 
         //DFS to determine a path from left to right and save the minimum value
@@ -211,12 +166,12 @@ public sealed partial class Day17 : Day
                 current = stack.Pop();
                 if (!visited.Contains(current))
                 {
+                    min = Math.Min(min, current.X);
                     if (current == leftPoint) break;
                     visited.Add(current);
                     foreach (var next in Neighbors(current).Where(Valid))
                     {
                         stack.Push(next);
-                        min = Math.Min(min, next.X);
                     }
                 }
             }
@@ -237,81 +192,107 @@ public sealed partial class Day17 : Day
             list.Add(new Point(point.X + 1, point.Y));
             return list;
         }
-
-
-        public class Block
-        {
-            public int Type { get; set; }
-            public int[] Occupied = new int[4];
-
-            public Block(int blockType)
-            {
-                Type = blockType;
-                if (Type == 0)
-                {
-                    Occupied[0] = 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4;
-                }
-                else if (Type == 1)
-                {
-                    Occupied[2] = 1 << 3;
-                    Occupied[1] = 1 << 2 | 1 << 3 | 1 << 4;
-                    Occupied[0] = 1 << 3;
-                }
-                else if (Type == 2)
-                {
-                    Occupied[2] = 1 << 2;
-                    Occupied[1] = 1 << 2;
-                    Occupied[0] = 1 << 2 | 1 << 3 | 1 << 4;
-                }
-                else if (Type == 3)
-                {
-                    Occupied[3] = 1 << 4;
-                    Occupied[2] = 1 << 4;
-                    Occupied[1] = 1 << 4;
-                    Occupied[0] = 1 << 4;
-                }
-                else if (Type == 4)
-                {
-                    Occupied[1] = 1 << 3 | 1 << 4;
-                    Occupied[0] = 1 << 3 | 1 << 4;
-                }
-            }
-
-            public bool Overlap(int[] rocks)
-            {
-                for (int i = 0; i < Math.Max(rocks.Length, Occupied.Length); i++)
-                {
-                    if ((rocks[i] & Occupied[i]) != 0) return true;
-                }
-                return false;
-            }
-
-            public bool MoveLeft()
-            {
-                if (Occupied.Any(row => (row & (1 << 6)) != 0))
-                    return false;
-                for (int i = 0; i < Occupied.Length; i++)
-                {
-                    Occupied[i] = Occupied[i] << 1;
-                }
-                return true;
-            }
-            public bool MoveRight()
-            {
-                if (Occupied.Any(row => (row & (1 << 0)) != 0))
-                    return false;
-                for (int i = 0; i < Occupied.Length; i++)
-                {
-                    Occupied[i] = Occupied[i] >> 1;
-                }
-                return true;
-            }
-        }
-
-        public static Tetris Parse(string[] lines)
-        {
-            return new Tetris(lines[0]);
-        }
-
     }
 }
+
+public class TetrisBlock
+{
+    public int Type { get; set; }
+    public int[] Occupied = new int[4];
+
+    public TetrisBlock(int blockType)
+    {
+        Type = blockType;
+        if (Type == 0)
+        {
+            Occupied[0] = 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4;
+        }
+        else if (Type == 1)
+        {
+            Occupied[2] = 1 << 3;
+            Occupied[1] = 1 << 2 | 1 << 3 | 1 << 4;
+            Occupied[0] = 1 << 3;
+        }
+        else if (Type == 2)
+        {
+            Occupied[2] = 1 << 2;
+            Occupied[1] = 1 << 2;
+            Occupied[0] = 1 << 2 | 1 << 3 | 1 << 4;
+        }
+        else if (Type == 3)
+        {
+            Occupied[3] = 1 << 4;
+            Occupied[2] = 1 << 4;
+            Occupied[1] = 1 << 4;
+            Occupied[0] = 1 << 4;
+        }
+        else if (Type == 4)
+        {
+            Occupied[1] = 1 << 3 | 1 << 4;
+            Occupied[0] = 1 << 3 | 1 << 4;
+        }
+    }
+
+    public bool Overlap(int[] rocks)
+    {
+        for (int i = 0; i < Math.Max(rocks.Length, Occupied.Length); i++)
+        {
+            if ((rocks[i] & Occupied[i]) != 0) return true;
+        }
+        return false;
+    }
+
+    public bool MoveLeft()
+    {
+        if (Occupied.Any(row => (row & (1 << 6)) != 0))
+            return false;
+        for (int i = 0; i < Occupied.Length; i++)
+        {
+            Occupied[i] = Occupied[i] << 1;
+        }
+        return true;
+    }
+
+    public bool MoveRight()
+    {
+        if (Occupied.Any(row => (row & (1 << 0)) != 0))
+            return false;
+        for (int i = 0; i < Occupied.Length; i++)
+        {
+            Occupied[i] = Occupied[i] >> 1;
+        }
+        return true;
+    }
+}
+
+public class TetrisState
+{
+    int[] Rocks;
+    int BlockType;
+    int WindIndex;
+
+    public TetrisState(int[] rocks, long blockCount, int blockType, int windIndex)
+    {
+        Rocks = rocks;
+        BlockType = blockType;
+        WindIndex = windIndex;
+    }
+
+    public static bool operator ==(TetrisState self, TetrisState other) => self.Equals(other);
+
+    public static bool operator !=(TetrisState self, TetrisState other) => !(self == other);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is TetrisState { } state)
+        {
+            return
+                BlockType == state.BlockType &&
+                WindIndex == state.WindIndex &&
+                Enumerable.SequenceEqual(Rocks, state.Rocks);
+        }
+        return false;
+    }
+
+    public override int GetHashCode() => Rocks.Length;
+};
