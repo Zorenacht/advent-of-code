7b05e730ab19c5ef6dda40918a97dcaf8d70e023
Renzo Baasdam <renzobaasdam@gmail.com>
2023-01-03 11:18:55 +0100

    Bits

---
+++ Tools/Bits.cs
@@ -0,0 +1,11 @@
+﻿namespace Tools.Geometry;
+
+public static class Bits
+{
+    public static long Extract(long bits, int start, int length)
+    {
+        long mask = (1 << length) - 1;
+        long translatedMask = mask << start;
+        return bits & translatedMask;
+    }
+}
