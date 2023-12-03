d9bdd400024c45b53dafe855f2d8a5032d3cbf60
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-03 14:40:19 +0100

    refactor: move 2022 to shared project

---
+++ AoC/AoC/2022/Day.cs
@@ -0,0 +1,17 @@
+namespace AoC_2022;
+
+public abstract class Day
+{
+    protected readonly string[] InputExample = Array.Empty<string>();
+    protected readonly string[] InputPart1;
+    protected readonly string[] InputPart2 = Array.Empty<string>();
+
+    protected Day()
+    {
+        var year = GetType().Namespace![^4..];
+        var className = GetType().Name;
+        Reader.TryReadLines(@$"{year}\Input\{className}-Example.txt", out InputExample);
+        Reader.TryReadLines(@$"{year}\Input\{className}-1.txt", out InputPart1);
+        InputPart2 = InputPart1;
+    }
+}
