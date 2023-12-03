3d4bfefe4815285939c2ff9af1d2b42a8d11e6da
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-03 21:23:05 +0100

    chore: 2023-04 setup

---
+++ AoC/2023/Day04.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using Tools;
+
+namespace AoC_2023;
+
+public sealed class Day04 : Day
+{
+    [Puzzle(answer: null)]
+    public int Part1()
+    {
+        var result = 0;
+        return result;
+    }
+
+    public string DigitsOrDot = "0123456789.";
+    public string Digits = "0123456789";
+
+    [Puzzle(answer: null)]
+    public int Part2()
+    {
+        var result = 0;
+        return result;
+    }
+}
