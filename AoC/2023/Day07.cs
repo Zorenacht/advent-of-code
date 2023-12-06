a1200781797a2aa7583d09e17e7e2a7a49501eb7
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-06 22:59:33 +0100

    chore: 2023-07 setup

---
+++ AoC/2023/Day07.cs
@@ -0,0 +1,20 @@
+namespace AoC_2023;
+
+public sealed class Day07 : Day
+{
+    [Puzzle(answer: null)]
+    public long Part1()
+    {
+        int result = 0;
+        var parsed = Input;
+        return result;
+    }
+
+    [Puzzle(answer: null)]
+    public long Part2()
+    {
+        int result = 0;
+        var parsed = Input;
+        return result;
+    }
+}
