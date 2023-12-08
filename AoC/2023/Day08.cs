2c944de5e1555db1c267ae1391a77539ef6afa75
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-08 07:36:08 +0100

    refactor: 2023-08

--- AoC/2023/Day08.cs
+++ AoC/2023/Day08.cs
@@ -13,8 +13,8 @@ public sealed class Day08 : Day
 
     private long MinSteps(Func<string,bool> startCondition, Func<string,bool> endCondition)
     {
-        var input = Input[0];
-        var parsed = Input[2..]
+        var sequence = Input[0];
+        var mapping = Input[2..]
             .Select(x =>
             {
                 var split = x.Split([" = (", ", ", ")"], StringSplitOptions.RemoveEmptyEntries);
@@ -22,24 +22,27 @@ public sealed class Day08 : Day
             })
             .ToDictionary();
 
-        var curr = parsed.Where(x => startCondition(x.Key)).Select(x => x.Key).ToArray();
-        var period = new int[curr.Length];
+        var currentNodes = mapping
+            .Where(x => startCondition(x.Key))
+            .Select(x => x.Key)
+            .ToArray();
+        var period = new int[currentNodes.Length];
         for (var i = 0; i < period.Length; i++)
         {
             int steps = 0;
-            var c = curr[i];
-            while (!endCondition(c))
+            var node = currentNodes[i];
+            while (!endCondition(node))
             {
-                var next = new List<string>();
-                var lr = input[steps % input.Length];
-                var first = parsed[c];
-                if (lr == 'L') c = first.Left;
-                if (lr == 'R') c = first.Right;
+                var lr = sequence[steps % sequence.Length];
+                if (lr == 'L') node = mapping[node].Left;
+                if (lr == 'R') node = mapping[node].Right;
                 steps++;
             }
             period[i] = steps;
         }
-        var minPeriod = period.Select(x => new BigInteger(x)).Aggregate((x, y) => x * y / BigInteger.GreatestCommonDivisor(x, y));
+        var minPeriod = period
+            .Select(x => new BigInteger(x))
+            .Aggregate((x, y) => x * y / BigInteger.GreatestCommonDivisor(x, y));
         return (long)minPeriod;
     }
 }
