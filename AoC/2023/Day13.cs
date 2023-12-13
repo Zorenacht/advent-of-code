b938f5851727eb75d5daa521cf007647282dcda5
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-13 06:59:31 +0100

    refactor: 2023-13

--- AoC/2023/Day13.cs
+++ AoC/2023/Day13.cs
@@ -12,12 +12,18 @@ namespace AoC_2023;
 public sealed class Day13 : Day
 {
     [Puzzle(answer: 405)]
-    public int Part1Example() => P1(InputExampleAsText);
+    public int Part1Example() => P2(InputExampleAsText,0);
 
     [Puzzle(answer: 35538)]
-    public int Part1() => P1(InputAsText);
+    public int Part1() => P2(InputAsText,0);
 
-    public int P1(string input)
+    [Puzzle(answer: 400)]
+    public int Part2Example() => P2(InputExampleAsText, 1);
+
+    [Puzzle(answer: 30442)]
+    public int Part2() => P2(InputAsText, 1);
+
+    public int P2(string input, int amountOfInvalid)
     {
         int result = 0;
         var boards = input.Split($"{Environment.NewLine}{Environment.NewLine}")
@@ -28,75 +34,7 @@ public sealed class Day13 : Day
             var mirrorCol = -1;
             for (int row = 1; row < board.Length; row++)
             {
-                bool valid = true;
-                for (int i = 0; i < Math.Min(row, board.Length - row); i++)
-                {
-                    for (int j = 0; j < board[0].Length; j++)
-                    {
-                        if (board[row+i][j] != board[row - i-1][j])
-                        {
-                            i = int.MaxValue - 10;
-                            j = int.MaxValue - 10;
-                            valid = false;
-                        }
-                    }
-                }
-                if (valid)
-                {
-                    mirrorRow = row;
-                    break;
-                }
-            }
-            for (int col = 1; col < board[0].Length; col++)
-            {
-                bool valid = true;
-                for (int i = 0; i < board.Length; i++)
-                {
-                    for (int j = 0; j < Math.Min(col, board[0].Length - col); j++)
-                    {
-                        if (board[i][col+j] != board[i][col - j - 1])
-                        {
-                            i = int.MaxValue - 10;
-                            j = int.MaxValue - 10;
-                            valid = false;
-                        }
-                    }
-                }
-                if (valid)
-                {
-                    mirrorCol = col;
-                    break;
-                }
-            }
-
-
-            if (mirrorRow > 0) result += mirrorRow * 100;
-            else if (mirrorCol > 0) result += mirrorCol;
-            else throw new Exception();
-        }
-        return result;
-    }
-
-    [Puzzle(answer: 400)]
-    public int Part2Example() => P2(InputExampleAsText);
-
-    [Puzzle(answer: 30442)]
-    public int Part2() => P2(InputAsText);
-
-    public int P2(string input)
-    {
-        int result = 0;
-        var boards = input.Split($"{Environment.NewLine}{Environment.NewLine}")
-            .Select(x => x.Split(Environment.NewLine)).ToList();
-        foreach (var b in boards)
-        {
-            var mirrorRow = -1;
-            var mirrorCol = -1;
-            for (int row = 1; row < b.Length; row++)
-            {
-                var board = b.Select(x => x.ToCharArray()).ToArray();
                 var smudges = new HashSet<(int, int)>();
-                bool smudgeFixed = false;
                 bool valid = true;
                 for (int i = 0; i < Math.Min(row, board.Length - row); i++)
                 {
@@ -104,10 +42,8 @@ public sealed class Day13 : Day
                     {
                         if (board[row + i][j] != board[row - i - 1][j])
                         {
-                            if (smudges.Count < 1)
+                            if (smudges.Count < amountOfInvalid)
                             {
-                                board[row + i][j] = board[row + i][j] == '#' ? '.' : '#';
-                                smudgeFixed = true;
                                 smudges.Add((i, j));
                             }
                             else
@@ -119,17 +55,15 @@ public sealed class Day13 : Day
                         }
                     }
                 }
-                if (valid && smudges.Count == 1)
+                if (valid && smudges.Count == amountOfInvalid)
                 {
                     mirrorRow = row;
                     break;
                 }
             }
-            for (int col = 1; col < b[0].Length; col++)
+            for (int col = 1; col < board[0].Length; col++)
             {
-                var board = b.Select(x => x.ToCharArray()).ToArray();
                 var smudges = new HashSet<(int, int)>();
-                bool smudgeFixed = false;
                 bool valid = true;
                 for (int i = 0; i < board.Length; i++)
                 {
@@ -137,9 +71,8 @@ public sealed class Day13 : Day
                     {
                         if (board[i][col + j] != board[i][col - j - 1])
                         {
-                            if (smudges.Count < 1)
+                            if (smudges.Count < amountOfInvalid)
                             {
-                                smudgeFixed = true;
                                 smudges.Add((i, j));
                             }
                             else
@@ -151,7 +84,7 @@ public sealed class Day13 : Day
                         }
                     }
                 }
-                if (valid && smudges.Count == 1)
+                if (valid && smudges.Count == amountOfInvalid)
                 {
                     mirrorCol = col;
                     break;
