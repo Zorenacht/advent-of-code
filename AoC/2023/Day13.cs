3dfd4385db5936f119aac556575ad6b7bfe4d33a
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-13 09:42:39 +0100

    refactor: 2023-13

--- AoC/2023/Day13.cs
+++ AoC/2023/Day13.cs
@@ -12,10 +12,10 @@ namespace AoC_2023;
 public sealed class Day13 : Day
 {
     [Puzzle(answer: 405)]
-    public int Part1Example() => P2(InputExampleAsText,0);
+    public int Part1Example() => P2(InputExampleAsText, 0);
 
     [Puzzle(answer: 35538)]
-    public int Part1() => P2(InputAsText,0);
+    public int Part1() => P2(InputAsText, 0);
 
     [Puzzle(answer: 400)]
     public int Part2Example() => P2(InputExampleAsText, 1);
@@ -34,28 +34,9 @@ public sealed class Day13 : Day
             var mirrorCol = -1;
             for (int row = 1; row < board.Length; row++)
             {
-                var smudges = new HashSet<(int, int)>();
-                bool valid = true;
-                for (int i = 0; i < Math.Min(row, board.Length - row); i++)
-                {
-                    for (int j = 0; j < board[0].Length; j++)
-                    {
-                        if (board[row + i][j] != board[row - i - 1][j])
-                        {
-                            if (smudges.Count < amountOfInvalid)
-                            {
-                                smudges.Add((i, j));
-                            }
-                            else
-                            {
-                                i = int.MaxValue - 10;
-                                j = int.MaxValue - 10;
-                                valid = false;
-                            }
-                        }
-                    }
-                }
-                if (valid && smudges.Count == amountOfInvalid)
+                var first = board[..row].Reverse().ToArray();
+                var second = board[row..];
+                if (Differences(first, second) == amountOfInvalid)
                 {
                     mirrorRow = row;
                     break;
@@ -63,40 +44,32 @@ public sealed class Day13 : Day
             }
             for (int col = 1; col < board[0].Length; col++)
             {
-                var smudges = new HashSet<(int, int)>();
-                bool valid = true;
-                for (int i = 0; i < board.Length; i++)
-                {
-                    for (int j = 0; j < Math.Min(col, board[0].Length - col); j++)
-                    {
-                        if (board[i][col + j] != board[i][col - j - 1])
-                        {
-                            if (smudges.Count < amountOfInvalid)
-                            {
-                                smudges.Add((i, j));
-                            }
-                            else
-                            {
-                                i = int.MaxValue - 10;
-                                j = int.MaxValue - 10;
-                                valid = false;
-                            }
-                        }
-                    }
-                }
-                if (valid && smudges.Count == amountOfInvalid)
+                var first = board.Select(x => new string(x[..col].Reverse().ToArray())).ToArray();
+                var second = board.Select(x => x[col..]).ToArray();
+                if (Differences(first, second) == amountOfInvalid)
                 {
                     mirrorCol = col;
                     break;
                 }
             }
-
-
             if (mirrorRow > 0) result += mirrorRow * 100;
             else if (mirrorCol > 0) result += mirrorCol;
             else throw new Exception();
         }
         return result;
+    }
+
+    private int Differences(string[] first, string[] second)
+    {
+        int unequal = 0;
+        for (int i = 0; i < Math.Min(first.Length, second.Length); i++)
+        {
+            for (int j = 0; j < Math.Min(first[0].Length, second[0].Length); j++)
+            {
+                if (first[i][j] != second[i][j]) unequal++;
+            }
+        }
+        return unequal;
     }
 
 }
