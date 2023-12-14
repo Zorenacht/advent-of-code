da7a87e291810b26e01c2f7dfb6e2cd0f80c121b
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-14 07:44:40 +0100

    refactor: 2023-14

--- AoC/2023/Day14.cs
+++ AoC/2023/Day14.cs
@@ -13,133 +13,61 @@ namespace AoC_2023;
 public sealed class Day14 : Day
 {
     [Puzzle(answer: 136)]
-    public int Part1Example() => P1(InputExample);
-
+    public int Part1Example() => NorthLoad(MoveAllNorth(InputExample));
     [Puzzle(answer: 111979)]
-    public int Part1() => P1(Input);
-    public int P1(string[] input)
-    {
-        int result = 0;
-        int count = 0;
-        for (int j = 0; j < input[0].Length; j++)
-        {
-            int lastTag = -1;
-            int os = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i][j] == 'O') count += input.Length - (lastTag + ++os);
-                if (input[i][j] == '#')
-                {
-                    lastTag = i;
-                    os = 0;
-                }
-            }
-        }
-        return count;
-    }
+    public int Part1() => NorthLoad(MoveAllNorth(Input));
 
     [Puzzle(answer: 64)]
     public long Part2Example() => P2(InputExample);
-
     [Puzzle(answer: 102055)]
     public long Part2() => P2(Input);
-
-    private bool Equal(string[] arr1, string[] arr2)
-    {
-        for (int i = 0; i < arr1.Length; i++)
-        {
-            for (int j = 0; j < arr1[0].Length; j++)
-            {
-                if (arr1[i][j] != arr2[i][j]) return false;
-            }
-        }
-        return true;
-    }
-
-    private int Load(string[] arr)
-    {
-        int count = 0;
-        for (int i = 0; i < arr.Length; i++)
-        {
-            for (int j = 0; j < arr[0].Length; j++)
-            {
-                if (arr[i][j] == 'O') count += arr.Length - i;
-            }
-        }
-        return count;
-    }
 
     public long P2(string[] input)
     {
         int result = 0;
         var copy = input.Select(x => Enumerable.Repeat('.', x.Length).ToArray()).ToArray();
-        Console.WriteLine(string.Join("\n", input.Select(x => new string(x.ToArray()))));
-        Console.WriteLine();
-        var cache = new List<(string[], long)>();
-        for (long count = 0; count <= 100000; count++)
+        var cache = new List<string[]>();
+        int iteration = 0;
+        while(true)
         {
             for (int turn = 0; turn < 4; turn++)
             {
-                for (int j = 0; j < input[0].Length; j++)
-                {
-                    int lastTag = -1;
-                    int os = 0;
-                    for (int i = 0; i < input.Length; i++)
-                    {
-                        if (input[i][j] == '#')
-                        {
-                            copy[i][j] = '#';
-                            lastTag = i;
-                            os = 0;
-                        }
-                        if (input[i][j] == 'O' && lastTag + os + 1 < input.Length)
-                        {
-                            copy[lastTag + ++os][j] = 'O';
-                        }
-                    }
-                }
-                input = RotateClockWise(copy.Select(x => new string(x)).ToArray());
-                copy = input.Select(x => Enumerable.Repeat('.', x.Length).ToArray()).ToArray();
+                input = RotateClockWise(MoveAllNorth(input));
             }
-            if (cache.Any(x => Equal(x.Item1, input)))
+            if (cache.Any(cached => Equal(cached, input)))
             {
-                cache.Add((input, count));
-                var test = cache.Select(x => Load(x.Item1)).ToList();
-                var ele = cache.First(x => Equal(x.Item1, input));
-                var index = ele.Item2 + (1000000000L - ele.Item2) % (count - ele.Item2);
-                var target = cache[(int)index - 1].Item1;
-                return Load(target);
+                var cycleStart = cache.Select((cached, index) => (cached, index)).First(x => Equal(x.Item1, input)).Item2;
+                var cycleLength = (iteration - cycleStart);
+                var index = cycleStart + (1000000000L - cycleStart) % cycleLength;
+                var target = cache[(int)index - 1];
+                return NorthLoad(target);
             }
-            else
-            {
-                cache.Add((input, count));
-            }
-            Console.WriteLine(string.Join("\n", input.Select(x => new string(x.ToArray()))));
-            Console.WriteLine();
+            cache.Add(input);
+            iteration++;
         }
-        /*for (int i = 0; i < input.Length; i++)
+        return result;
+    }
+
+    public string[] MoveAllNorth(string[] input)
+    {
+        var copy = input.Select(x => Enumerable.Repeat('.', x.Length).ToArray()).ToArray();
+        for (int j = 0; j < input[0].Length; j++)
         {
-            int lastTag = -1;
-            int os = 0;
-            for (int j = 0; j < input[0].Length; j++)
+            int wall = -1;
+            for (int i = 0; i < input.Length; i++)
             {
                 if (input[i][j] == '#')
                 {
                     copy[i][j] = '#';
-                    lastTag = j;
-                    os = 0;
+                    wall = i;
                 }
-                if (input[i][j] == 'O' && lastTag + os + 1 < input[0].Length)
+                if (input[i][j] == 'O' && wall + 1 < input.Length)
                 {
-                    copy[i][lastTag + ++os] = 'O';
+                    copy[++wall][j] = 'O';
                 }
             }
         }
-        input = copy.Select(x => new string(x.ToArray())).ToArray();
-        copy = input.Select(x => Enumerable.Repeat('.', x.Length).ToList()).ToArray();*//*
-        Console.WriteLine(string.Join("\n", input.Select(x => new string(x.ToArray()))));
-        Console.WriteLine();*/
-        return result;
+        return copy.Select(x => new string(x)).ToArray();
     }
 
     public string[] RotateClockWise(string[] input)
@@ -162,5 +90,27 @@ public sealed class Day14 : Day
             }
         }
         return vertical.Select(x => new string(x)).ToArray();
+    }
+
+    private bool Equal(string[] arr1, string[] arr2)
+    {
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+
+    private int NorthLoad(string[] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr[0].Length; j++)
+            {
+                if (arr[i][j] == 'O') count += arr.Length - i;
+            }
+        }
+        return count;
     }
 }
