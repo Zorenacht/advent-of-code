69339ce2f3039e26f557c190b912680756087329
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-14 22:02:04 +0100

    chore: 2023-15 setup

---
+++ AoC/2023/Day15.cs
@@ -0,0 +1,41 @@
+using AoC;
+using MathNet.Numerics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Tools.Geometry;
+using Tools.Shapes;
+
+namespace AoC_2023;
+
+public sealed class Day15 : Day
+{
+    [Puzzle(answer: null)]
+    public long Part1Example() => Part1(InputExample);
+
+    [Puzzle(answer: null)]
+    public long Part1() => Part1(Input);
+
+    public long Part1(string[] input)
+    {
+        long result = 0;
+        return result;
+    }
+
+
+
+    [Puzzle(answer: null)]
+    public long Part2Example() => Part2(InputExample);
+
+    [Puzzle(answer: null)]
+    public long Part2() => Part2(Input);
+
+    public long Part2(string[] input)
+    {
+        long result = 0;
+        return result;
+    }
+
+}
