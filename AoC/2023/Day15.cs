8590b982277e217bdf4db60f40439d90c2b7c863
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-15 07:14:28 +0100

    feat: 2023-15 refactored

--- AoC/2023/Day15.cs
+++ AoC/2023/Day15.cs
@@ -12,30 +12,79 @@ namespace AoC_2023;
 
 public sealed class Day15 : Day
 {
-    [Puzzle(answer: null)]
-    public long Part1Example() => Part1(InputExample);
+    [Puzzle(answer: 1320)]
+    public long Part1Example() => InputExampleAsText.Split(",").Sum(word => HASH(word));
 
-    [Puzzle(answer: null)]
-    public long Part1() => Part1(Input);
+    [Puzzle(answer: 517015)]
+    public long Part1() => InputAsText.Split(",").Sum(word => HASH(word));
 
-    public long Part1(string[] input)
+    private int[] Rows = [-1, 0, 1, 0];
+    private int[] Cols = [0, -1, 0, 1];
+
+    [Puzzle(answer: 145)]
+    public long Part2Example() => Part2(InputExampleAsText);
+
+    [Puzzle(answer: 286104)]
+    public long Part2() => Part2(InputAsText);
+
+    private record Lens(string Label)
     {
-        long result = 0;
-        return result;
+        public int Strength { get; set; }
     }
 
-
-
-    [Puzzle(answer: null)]
-    public long Part2Example() => Part2(InputExample);
-
-    [Puzzle(answer: null)]
-    public long Part2() => Part2(Input);
-
-    public long Part2(string[] input)
+    private int HASH(string word)
     {
-        long result = 0;
-        return result;
+        int sum = 0;
+        foreach (char ch in word)
+        {
+            sum = (sum + (int)ch) * 17;
+            sum %= 256;
+        }
+        return sum;
     }
 
+    public long Part2(string input)
+    {
+        List<List<Lens>> boxes = Enumerable.Range(0, 256).Select(x => new List<Lens>()).ToList();
+
+        var split = input.Split(",");
+        foreach (var cmd in split)
+        {
+            if (cmd[^1] == '-')
+            {
+                var remove = cmd[..^1];
+                int box = HASH(remove);
+                for (int i = 0; i < boxes[box].Count; i++)
+                {
+                    if (boxes[box][i].Label == remove)
+                    {
+                        boxes[box].RemoveAt(i);
+                    }
+                }
+            }
+            else
+            {
+                var replace = cmd.Split("=");
+                int box = HASH(replace[0]);
+                int value = int.Parse(replace[1]);
+                if (boxes[box].FirstOrDefault(x => x.Label == replace[0]) is { } res)
+                {
+                    res.Strength = value;
+                }
+                else
+                {
+                    boxes[box].Add(new Lens(replace[0]) { Strength = value });
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            for (int j = 0; j < boxes[i].Count; j++)
+            {
+                result += (i + 1) * (j + 1) * boxes[i][j].Strength;
+            }
+        }
+        return result;
+    }
 }
