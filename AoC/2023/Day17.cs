a4d2d92d04ba3e6ada94848bb89ff8f415d6290d
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-16 23:23:29 +0100

    chore: set up 2023-17

---
+++ AoC/2023/Day17.cs
@@ -0,0 +1,29 @@
+using MathNet.Numerics;
+
+namespace AoC_2023;
+
+public sealed class Day17 : Day
+{
+    [Puzzle(answer: null)]
+    public int Part1Example() => P1(InputExample);
+
+    [Puzzle(answer: null)]
+    public int Part1() => P1(Input);
+
+    public int P1(string[] input)
+    {
+        return 0;
+    }
+
+    [Puzzle(answer: null)]
+    public int Part2Example() => Part2(InputExample);
+
+    [Puzzle(answer: null)]
+    public int Part2() => Part2(Input);
+
+    public int Part2(string[] input)
+    {
+        return 0;
+    }
+
+}
