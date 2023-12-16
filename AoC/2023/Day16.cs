7dfe0e52dca67bbed68976314b2009681bb14bbd
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-16 07:35:31 +0100

    feat: 2023-16 initial

--- AoC/2023/Day16.cs
+++ AoC/2023/Day16.cs
@@ -1,5 +1,6 @@
 using AoC;
 using MathNet.Numerics;
+using NUnit.Framework.Constraints;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
@@ -12,10 +13,10 @@ namespace AoC_2023;
 
 public sealed class Day16 : Day
 {
-    [Puzzle(answer: null)]
+    [Puzzle(answer: 46)]
     public int Part1Example() => P1(InputExample);
 
-    [Puzzle(answer: null)]
+    [Puzzle(answer: 7728)]
     public int Part1() => P1(Input);
 
     Direction[] dirs = [
@@ -28,56 +29,120 @@ public sealed class Day16 : Day
         Direction.SE,
         Direction.NE];
 
-    public int P1(string[] input)
+    private IEnumerable<Complex32> Next(char ch, Complex32 dir)
     {
-        int result = 0;
-        var parse = input.Select(x =>
-        {
-            var split1 = x.Split(" ", StringSplitOptions.None);
-            var split2 = x.Split(" ", StringSplitOptions.None);
-            var split3 = x.Split(" ", StringSplitOptions.None);
-            var kv = new KeyValuePair<string, string>(split1[0], split2[0]);
-            return x;
-        });
-        var board = input.AddBorder('*');
-        for (int i = 0; i < board.Length; i++)
-        {
-            for (int j = 0; j < board[0].Length; j++)
-            {
-            }
-        }
-        var current = new List<Point>();
-        var visited = new HashSet<Point>();
-        while (current.Count > 0)
-        {
-            var next = new List<Point>();
-            foreach (var curr in current)
-            {
-                if (visited.Contains(curr)) continue;
-                next.AddRange(dirs.Select(x => curr.NeighborV(x)));
-            }
-        }
-        return result;
+        if (ch == '.') yield return dir;
+        if (ch == '|' && (dir == 1 || dir == -1)) { yield return new Complex32(0, 1); yield return new Complex32(0, -1); }
+        if (ch == '|' && (dir.Imaginary == 1 || dir.Imaginary == -1)) yield return dir;
+        if (ch == '-' && (dir == 1 || dir == -1)) yield return dir;
+        if (ch == '-' && (dir.Imaginary == 1 || dir.Imaginary == -1)) { yield return new Complex32(1, 0); yield return new Complex32(-1, 0); }
+        if (ch == '/' && (dir == 1 || dir == -1)) yield return dir * new Complex32(0, 1);
+        if (ch == '/' && (dir.Imaginary == 1 || dir.Imaginary == -1)) yield return dir * new Complex32(0, -1);
+        if (ch == '\\' && (dir == 1|| dir == -1)) yield return dir * new Complex32(0, -1);
+        if (ch == '\\' && (dir.Imaginary == 1 || dir.Imaginary == -1)) yield return dir * new Complex32(0, 1);
     }
 
-    [Puzzle(answer: null)]
+    /*private IEnumerable<(int, int)> Next(char ch, (int, int) dir)
+    {
+        if (ch == '.') yield return dir;
+        if (ch == '|' && dir == (1, 0) || dir == (-1, 0)) yield return (0, 1); yield return (0, -1);
+        if (ch == '|' && dir == (0, 1) || dir == (0, -1)) yield return dir;
+        if (ch == '-' && dir == (1, 0) || dir == (-1, 0)) yield return dir;
+        if (ch == '-' && dir == (0, 1) || dir == (0, -1)) yield return (1, 0); yield return (-1, 0);
+        if( ch == '/' && dir == (0, -1) || dir == (0, 1))
+        if( ch == '/')
+        if( ch == '\')
+        if( ch == '\')
+    }*/
+
+    public int P1(string[] input)
+    {
+        var board = input.AddBorder('*').Reverse().ToArray();
+        var copy = board.Select(x => x.ToArray()).ToArray();
+        var dir = new Complex32(1, 0);
+        var current = new Complex32(1, board.Length-2);
+        var nexts = new Queue<(Complex32 Current, Complex32 Dir)>();
+        nexts.Enqueue((current, dir));
+        var visited = new HashSet<(Complex32 Current, Complex32 Dir)>();
+        while (nexts.TryPeek(out var _))
+        {
+            var next = nexts.Dequeue();
+            current = next.Current;
+            dir = next.Dir;
+            var value = board[(int)current.Imaginary][(int)current.Real];
+            if ("*".Contains(value) || visited.Contains((current,dir))) continue;
+            var newPoints = Next(value, dir).ToArray();
+            foreach(var p in newPoints.Select(d => (current + d, d)))
+            {
+                nexts.Enqueue(p);
+            }
+            copy[(int)current.Imaginary][(int)current.Real] = '#';
+            visited.Add((current, dir));
+
+            /*for(int i =0; i < copy.Length; i++)
+            {
+                Console.WriteLine(string.Join("", copy.Skip(copy.Length-i-1).First()));
+            }*/
+            
+        }
+        return copy.Sum(r => r.Count(ch => ch == '#'));
+    }
+
+    [Puzzle(answer: 51)]
     public int Part2Example() => Part2(InputExample);
 
-    [Puzzle(answer: null)]
+    [Puzzle(answer: 8061)]
     public int Part2() => Part2(Input);
 
     public int Part2(string[] input)
     {
-        int result = 0;
-        var parse = input.Select(x =>
+        var board = input.AddBorder('*').Reverse().ToArray();
+        var allVisited = new HashSet<(Complex32 Current, Complex32 Dir)>();
+        var bottom = board[1][1..^1].Select((x, index) => new Complex32(index + 1, 1));
+        var top = board[board.Length - 2][1..^1].Select((x, index) => new Complex32(index + 1, board.Length - 2));
+        var left = board.Select((x, index) => new Complex32(1, index)).ToArray()[1..^1];
+        var right = board.Select((x, index) => new Complex32(board.Length-2, index)).ToArray()[1..^1];
+        var dirs = new[] { new Complex32(0, 1), new Complex32(0, -1), new Complex32(1, 0), new Complex32(-1, 0) };
+        var borders = top.Union(left).Union(right).Union(bottom)
+            .SelectMany(x => dirs
+                .Select(y => (x,y)))
+            .ToHashSet();
+        int max = 0;
+        while (borders.Any())
         {
-            var split1 = x.Split(" ", StringSplitOptions.None);
-            var split2 = x.Split(" ", StringSplitOptions.None);
-            var split3 = x.Split(" ", StringSplitOptions.None);
-            var kv = new KeyValuePair<string, string>(split1[0], split2[0]);
-            return x;
-        });
-        return result;
+            var copy = board.Select(x => x.ToArray()).ToArray();
+            var border = borders.First();
+            var current = border.Item1;
+            var dir = border.Item2;
+            var nexts = new Queue<(Complex32 Current, Complex32 Dir)>();
+            nexts.Enqueue((current, dir));
+            var visited = new HashSet<(Complex32 Current, Complex32 Dir)>();
+            while (nexts.TryPeek(out var _))
+            {
+                var next = nexts.Dequeue();
+                current = next.Current;
+                dir = next.Dir;
+                var value = board[(int)current.Imaginary][(int)current.Real];
+                if ("*".Contains(value) || visited.Contains((current, dir)))
+                {
+                    visited.Add((current, dir));
+                    continue;
+                }
+                var newPoints = Next(value, dir).ToArray();
+                foreach (var p in newPoints.Select(d => (current + d, d)))
+                {
+                    nexts.Enqueue(p);
+                }
+                copy[(int)current.Imaginary][(int)current.Real] = '#';
+                visited.Add((current, dir));
+            }
+            var count = copy.Sum(r => r.Count(ch => ch == '#'));
+            if (max < count) max = count;
+
+            allVisited.ExceptWith(visited);
+            borders.ExceptWith(visited);
+        }
+        return max;
     }
 
 }
--- AoC/2023/Program.cs
+++ AoC/2023/Program.cs
@@ -1,4 +1,4 @@
 using AoC_2023;
 
-var result = new Day14().Part2Example();
+var result = new Day16().Part2Example();
 Console.WriteLine(result);
