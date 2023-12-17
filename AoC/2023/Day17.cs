d7c48077a5130b1a788b98178edcfa98ae0f5615
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-17 07:46:43 +0100

    refactor: 2023-17

--- AoC/2023/Day17.cs
+++ AoC/2023/Day17.cs
@@ -9,15 +9,20 @@ namespace AoC_2023;
 public sealed class Day17 : Day
 {
     [Puzzle(answer: 102)]
-    public int Part1Example() => P1(InputExample);
+    public int Part1Example() => Part2(InputExample, 0, 3);
 
     [Puzzle(answer: 1128)]
-    public int Part1() => P1(Input);
+    public int Part1() => Part2(Input, 0, 3);
+
+    [Puzzle(answer: 94)]
+    public int Part2Example() => Part2(InputExample, 4, 10);
+
+    [Puzzle(answer: 1268)]
+    public int Part2() => Part2(Input, 4, 10);
 
     public record State(Complex32 Point, Complex32 Dir, int Count);
 
-    //not 1129
-    public int P1(string[] input)
+    public int Part2(string[] input, int min, int max)
     {
         var board = input.AddBorder('*').Reverse().ToArray();
         var pq = new PriorityQueue<State, int>();
@@ -35,18 +40,18 @@ public sealed class Day17 : Day
             var cost = boardValue - '0';
             if (boardValue == '*' || dict.ContainsKey(state)) continue;
             //if (state.Point == new Complex32(board[0].Length - 2, 1)) return state.Distance + cost;
-            pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, 1), state.Dir * new Complex32(0, 1), 1),
+            if (state.Count >= min) pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, 1), state.Dir * new Complex32(0, 1), 1),
                 priority + cost);
-            pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, -1), state.Dir * new Complex32(0, -1), 1),
+            if (state.Count >= min) pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, -1), state.Dir * new Complex32(0, -1), 1),
                 priority + cost);
-            if (state.Count < 3) pq.Enqueue(new State(state.Point + state.Dir, state.Dir, state.Count+1),
+            if (state.Count < max) pq.Enqueue(new State(state.Point + state.Dir, state.Dir, state.Count + 1),
                 priority + cost);
-            //if (Count < 3) yield return new Node<St>(new St(Point + Dir, Dir, Count + 1), initialDistance + );
             dict.Add(state, priority + cost);
         }
 
-        return dict.Where(x => x.Key.Point == new Complex32(board[0].Length - 2, 1)).Min(x => x.Value);
+        return dict.Where(x => x.Key.Point == new Complex32(board[0].Length - 2, 1) && x.Key.Count >= min).Min(x => x.Value);
     }
+
 
     private static void Print(string[] board, State state)
     {
@@ -75,41 +80,4 @@ public sealed class Day17 : Day
             Console.WriteLine();
         }
     }
-
-    [Puzzle(answer: 94)]
-    public int Part2Example() => Part2(InputExample);
-
-    [Puzzle(answer: 1268)]
-    public int Part2() => Part2(Input);
-
-    public int Part2(string[] input)
-    {
-        var board = input.AddBorder('*').Reverse().ToArray();
-        var pq = new PriorityQueue<State, int>();
-        var start = new Complex32(1, board.Length - 2);
-        pq.Enqueue(new State(start + new Complex32(1, 0), new Complex32(1, 0), 1), 0);
-        pq.Enqueue(new State(start + new Complex32(0, -1), new Complex32(0, -1), 1), 0);
-        var dict = new Dictionary<State, int>();
-        while (pq.Count > 0)
-        {
-            pq.TryDequeue(out var state, out var priority);
-            //Console.WriteLine(string.Join("\n", board.Select(x => x)));
-            //Console.WriteLine();
-            //Print(board, state);
-            var boardValue = board[(int)state!.Point.Imaginary][(int)state.Point.Real];
-            var cost = boardValue - '0';
-            if (boardValue == '*' || dict.ContainsKey(state)) continue;
-            //if (state.Point == new Complex32(board[0].Length - 2, 1)) return state.Distance + cost;
-            if (state.Count >= 4) pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, 1), state.Dir * new Complex32(0, 1), 1),
-                priority + cost);
-            if (state.Count >= 4) pq.Enqueue(new State(state.Point + state.Dir * new Complex32(0, -1), state.Dir * new Complex32(0, -1), 1),
-                priority + cost);
-            if (state.Count < 10) pq.Enqueue(new State(state.Point + state.Dir, state.Dir, state.Count + 1),
-                priority + cost);
-            dict.Add(state, priority + cost);
-        }
-
-        return dict.Where(x => x.Key.Point == new Complex32(board[0].Length - 2, 1)).Min(x => x.Value);
-    }
-
 }
