c98d2b1d869a0204a3a0d1c682332f95478372cf
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-17 20:59:13 +0100

    refactor: move next states logic to record

--- AoC/2023/Day17.cs
+++ AoC/2023/Day17.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using static AoC_2023.Day17;
 
 namespace AoC_2023;
 
@@ -16,32 +17,40 @@ public sealed class Day17 : Day
     [Puzzle(answer: 1268)]
     public int Part2() => Part2(Input, 4, 10);
 
-    public record State(Complex Point, Complex Dir, int Count);
+    public record State(Complex Point, Complex Dir, int Count)
+    {
+        public IEnumerable<State> Next(int min, int max)
+        {
+            if (Count < max) yield return new State(Point + Dir, Dir, Count + 1);
+            if (min <= Count) yield return new State(Point + Dir * new Complex(0, +1), Dir * new Complex(0, +1), 1);
+            if (min <= Count) yield return new State(Point + Dir * new Complex(0, -1), Dir * new Complex(0, -1), 1);
+        }
+    };
 
     public int Part2(string[] input, int min, int max)
     {
         var board = input.AddBorder('*').Reverse().ToArray();
         var start = new Complex(1, board.Length - 2);
-        var end = new Complex(board[0].Length - 2, 1);
+        var end = new Complex(board[0].Length/2 - 2, 1);
         var pq = new PriorityQueue<State, int>();
         pq.Enqueue(new State(start + new Complex(1, 0), new Complex(1, 0), 1), 0);
         pq.Enqueue(new State(start + new Complex(0, -1), new Complex(0, -1), 1), 0);
         var visited = new HashSet<State>();
+        int count = 0;
         while (pq.Count > 0)
         {
+            count++;
             pq.TryDequeue(out var state, out var priority);
             var boardValue = board[(int)state!.Point.Imaginary][(int)state.Point.Real];
             var cost = boardValue - '0';
-            if (state.Point == end) return priority + cost;
-            if (boardValue == '*' || visited.Contains(state)) continue;
-
-            if (state.Count < max) pq.Enqueue(new State(state.Point + state.Dir, state.Dir, state.Count + 1), priority + cost);
-            if (state.Count >= min)
+            if (state.Point == end)
             {
-                pq.Enqueue(new State(state.Point + state.Dir * new Complex(0, +1), state.Dir * new Complex(0, +1), 1), priority + cost);
-                pq.Enqueue(new State(state.Point + state.Dir * new Complex(0, -1), state.Dir * new Complex(0, -1), 1), priority + cost);
+                Console.WriteLine(pq.Count);
+                Console.WriteLine(count);
+                return priority + cost;
             }
-
+            if (boardValue == '*' || visited.Contains(state)) continue;
+            foreach (var next in state.Next(min,max)) pq.Enqueue(next, priority + cost);
             visited.Add(state);
         }
         throw new Exception("Endpoint was not reached!");
