938c76327d6e330595e2cad33044a3c3bbf7f461
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-18 12:18:05 +0100

    feat: 2023-18 initial

--- AoC/2023/Day18.cs
+++ AoC/2023/Day18.cs
@@ -72,11 +72,11 @@ public sealed class Day18 : Day
         return border.Count + inside.Count;
     }
 
-    [Puzzle(answer: null)]
-    public int Part2Example() => Part2(InputExample);
+    [Puzzle(answer: 952408144115)]
+    public long Part2Example() => Part2(InputExample);
 
-    [Puzzle(answer: null)]
-    public int Part2() => Part2(Input);
+    [Puzzle(answer: 67622758357096)]
+    public long Part2() => Part2(Input);
 
 
     private Complex Dir(string c) => c switch
@@ -88,7 +88,7 @@ public sealed class Day18 : Day
         _ => throw new Exception()
     };
 
-    public int Part2(string[] input)
+    public long Part2(string[] input)
     {
         var parse = input.Select<string, (int Steps, Complex Dir)>(x =>
         {
@@ -101,7 +101,7 @@ public sealed class Day18 : Day
             return (Convert.ToInt32(split[1..^1], 16), dir);
         }).ToList();
 
-
+        //create grid with reduced coordinates
         var location = Complex.Zero;
         var horizontal = new HashSet<int>() { 0 };
         var vertical = new HashSet<int>() { 0 };
@@ -111,13 +111,15 @@ public sealed class Day18 : Day
             horizontal.Add((int)location.Real);
             vertical.Add((int)location.Imaginary);
         }
-        var hValues = horizontal.ToList().OrderBy(x => x).ToList();
-        var vValues = vertical.ToList().OrderBy(x => x).ToList();
+        var hValues = horizontal.ToList().OrderBy(x => x).ToList().SelectMany((x, i) => i < horizontal.Count - 1 ? new[] { x, int.MaxValue } : new[] { x }).ToList();
+        var vValues = vertical.ToList().OrderBy(x => x).ToList().SelectMany((x, i) => i < vertical.Count - 1 ? new[] { x, int.MaxValue } : new[] { x }).ToList();
 
         var grid = Enumerable.Repeat(false, vValues.Count).Select(x => Enumerable.Repeat(false, hValues.Count).ToArray()).ToArray();
 
-        int rowIndex = 0;
-        int colIndex = 0;
+        //grid with borders
+        var border = new HashSet<Complex>() { };
+        int rowIndex = vValues.IndexOf(0);
+        int colIndex = hValues.IndexOf(0);
         foreach (var cmd in parse)
         {
             location += cmd.Steps * cmd.Dir;
@@ -126,6 +128,7 @@ public sealed class Day18 : Day
                 while (hValues[colIndex] != location.Real)
                 {
                     colIndex += Math.Sign(cmd.Dir.Real);
+                    border.Add(new Complex(colIndex, rowIndex));
                     grid[rowIndex][colIndex] = true;
                 }
             }
@@ -134,22 +137,71 @@ public sealed class Day18 : Day
                 while (vValues[rowIndex] != location.Imaginary)
                 {
                     rowIndex += Math.Sign(cmd.Dir.Imaginary);
+                    border.Add(new Complex(colIndex, rowIndex));
                     grid[rowIndex][colIndex] = true;
                 }
             }
         }
 
-        Print(grid);
+        //flood fill
+        var outside = new HashSet<Complex>() { Complex.Zero };
+        var inside = new HashSet<Complex>() { };
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                var loc = new Complex(j, i);
+                if (border.Contains(loc) || outside.Contains(loc) || inside.Contains(loc)) continue;
+                var que = new Queue<Complex>(); que.Enqueue(loc);
+                bool foundOutside = false;
+                var flood = new HashSet<Complex>();
+                while (que.Count > 0)
+                {
+                    var ele = que.Dequeue();
+                    if (flood.Contains(ele)) continue;
+                    foreach (var ch in "URDL")
+                    {
+                        var temp = ele + Dir(new string(ch, 1));
+                        if (!(temp.Real >= 0 && temp.Real < grid[0].Length && temp.Imaginary >= 0 && temp.Imaginary < grid.Length)) foundOutside = true;
+                        else if (!border.Contains(temp) && !flood.Contains(temp))
+                        {
+                            que.Enqueue(temp);
+                        }
+                    }
+                    flood.Add(ele);
+                }
+                if (foundOutside) outside.UnionWith(flood);
+                else { 
+                    foreach(var ele in flood) grid[(int)ele.Imaginary][(int)ele.Real] = true;
+                    inside.UnionWith(flood); 
+                }
+            }
+        }
 
-        return -100;
+        //count interior
+        long interiorCount = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                var loc = new Complex(j, i);
+                if (!inside.Contains(loc)) continue;
+                long verticalLength = vValues[i] == int.MaxValue ? vValues[i + 1] - vValues[i - 1] - 1 : 1;
+                long horizontalLength = hValues[j] == int.MaxValue ? hValues[j + 1] - hValues[j - 1] - 1 : 1;
+                interiorCount += horizontalLength * verticalLength;
+            }
+        }
+        long borderCount = parse.Sum(x => x.Steps);
+        return interiorCount + borderCount;
     }
 
     private static void Print(bool[][] board)
     {
-        for (int i = board.Length-1; i >= 0; i--)
+        for (int i = board.Length - 1; i >= 0; i--)
         {
             for (int j = 0; j < board[0].Length; j++)
             {
+
                 if (board[i][j])
                 {
                     Console.BackgroundColor = ConsoleColor.Blue;
@@ -160,5 +212,6 @@ public sealed class Day18 : Day
             }
             Console.WriteLine();
         }
+        Console.WriteLine();
     }
 }
--- AoC/2023/Program.cs
+++ AoC/2023/Program.cs
@@ -1,4 +1,4 @@
 using AoC_2023;
 
-var result = new Day18().Part1();
+var result = new Day18().Part2Example();
 Console.WriteLine(result);
