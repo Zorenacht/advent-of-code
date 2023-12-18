4196186dc28286182e641c1604b81fd2371c61e2
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-18 19:45:01 +0100

    perf: use matrices 2023-12

--- AoC/2023/Day12.cs
+++ AoC/2023/Day12.cs
@@ -1,3 +1,5 @@
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 namespace AoC_2023;
 
 public sealed class Day12 : Day
@@ -21,51 +23,80 @@ public sealed class Day12 : Day
 
     public class HotSprings
     {
-        private string template;
-        private int[] sequence;
+        private readonly string _singleTemplate;
+        private readonly int[] _singleSequence;
+        private int _repeats;
+        private readonly string _template;
+        private readonly int[] _sequence;
+        private readonly Dictionary<string, Matrix<double>> _cache;
 
         public HotSprings(string line, int repeat)
         {
-            var singleTemplate = line.Split()[0];
-            var singleSequence = line.Split()[1].Split(",").Select(x => int.Parse(x)).ToArray();
-            template = string.Join("?", Enumerable.Repeat(singleTemplate, repeat));
-            sequence = Enumerable.Repeat(singleSequence, repeat).SelectMany(x => x).ToArray();
+            _singleTemplate = line.Split()[0];
+            _singleSequence = line.Split()[1].Split(",").Select(x => int.Parse(x)).ToArray();
+            _repeats = repeat;
+            _template = string.Join("?", Enumerable.Repeat(_singleTemplate, repeat));
+            _sequence = Enumerable.Repeat(_singleSequence, repeat).SelectMany(x => x).ToArray();
+            _cache = new Dictionary<string, Matrix<double>>();
+
         }
 
-        // Time complexity: O(template.Length * sequence.Max * sequence.Length)
-        // Space complexity: O(sequence.Max * sequence.Length)
+        private Matrix<double> Power(Matrix<double> matrix, int power)
+        {
+            if (power == 0) return Matrix<double>.Build.SparseIdentity(matrix.RowCount);
+            if (power == 1) return matrix;
+            if (power % 2 == 0)
+            {
+                return Power(matrix * matrix, power / 2);
+            }
+            return matrix * Power(matrix, (power - 1) / 2);
+        }
+
+        private Matrix<double> FromTemplate(string template)
+        {
+            if (template.Length == 1) return _cache[template];
+            if(_cache.ContainsKey(template)) return _cache[template];
+            return FromTemplate(template[..(template.Length / 2)]) * FromTemplate(template[(template.Length / 2)..]);
+        }
+
         public long Arrangements()
         {
-            var states = sequence.Select(x => Enumerable.Repeat(0L, x + 1).ToList()).ToList();
-            states.Add(new List<long>() { 0 });
-
-            states[0][0] = 1;
-            for (int templateIndex = 0; templateIndex < template.Length; templateIndex++)
+            var states = _sequence.Sum(l => l + 1);
+            var indices = new int[_sequence.Length + 1];
+            for (int i = 1; i <= _sequence.Length; i++)
             {
-                var next = sequence.Select(x => Enumerable.Repeat(0L, x + 1).ToList()).ToList();
-                next.Add(new List<long>() { 0 });
-                var ch = template[templateIndex];
-                if (ch == '.' || ch == '?')
-                {
-                    for (int i = 0; i < states.Count; i++)
-                    {
-                        next[i][0] += states[i][0];
-                        if (i < states.Count - 1) next[i + 1][0] += states[i][^1];
-                    }
-                }
-                if (ch == '#' || ch == '?')
-                {
-                    for (int i = 0; i < states.Count; i++)
-                    {
-                        for (int j = 0; j < states[i].Count - 1; j++)
-                        {
-                            next[i][j + 1] += states[i][j];
-                        }
-                    }
-                }
-                states = next;
+                indices[i] = indices[i - 1] + _sequence[i - 1] + 1;
             }
-            return states[^2][^1] + states[^1][^1];
+            var dotMatrix = Matrix<double>.Build.Sparse(indices[^1] + 1, indices[^1] + 1);
+            dotMatrix[0, 0] = 1;
+            for (int i = 1; i < indices.Length; i++)
+            {
+                int row = indices[i];
+                dotMatrix[row - 1, row] = 1;
+                dotMatrix[row, row] = 1;
+            }
+            var hashMatrix = Matrix<double>.Build.Sparse(indices[^1] + 1, indices[^1] + 1);
+            for (int i = 0; i < indices.Length - 1; i++)
+            {
+                int row = indices[i];
+                int nextRow = indices[i + 1];
+                for (int j = row; j < nextRow - 1; j++)
+                {
+                    hashMatrix[j, j + 1] = 1;
+                }
+            }
+            var questionMatrix = dotMatrix + hashMatrix;
+
+            _cache["."] = dotMatrix;
+            _cache["#"] = hashMatrix;
+            _cache["?"] = questionMatrix;
+
+            var singleMatrix = FromTemplate(_singleTemplate);
+            var mat = singleMatrix * Power(questionMatrix * singleMatrix, _repeats-1);
+            var vector = Vector<double>.Build.Sparse(indices[^1] + 1);
+            vector[0] = 1;
+            var result = vector * mat;
+            return (long)(result[^1] + result[^2]);
         }
     }
 }
--- AoC/2023/Program.cs
+++ AoC/2023/Program.cs
@@ -1,4 +1,15 @@
 using AoC_2023;
+using System.Diagnostics;
 
-var result = new Day18().Part2Example();
-Console.WriteLine(result);
+int tries = 100;
+long accu = 0;
+var sw = new Stopwatch();
+for (int i = 0; i < tries; i++)
+{
+    var start = sw.ElapsedMilliseconds;
+    sw.Start();
+    var result = new Day12().Part2();
+    sw.Stop();
+    accu += sw.ElapsedMilliseconds - start;
+}
+Console.WriteLine(accu / tries);
