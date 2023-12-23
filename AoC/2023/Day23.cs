9c0e96d3a4bbae9b0dc12ae1144aa5088c32e968
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-23 15:29:23 +0100

    refactor: 2023-23

--- AoC/2023/Day23.cs
+++ AoC/2023/Day23.cs
@@ -2,6 +2,7 @@ using FluentAssertions.Equivalency.Steps;
 using MathNet.Numerics;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework.Constraints;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Drawing;
@@ -17,113 +18,78 @@ namespace AoC_2023;
 public sealed class Day23 : Day
 {
     [Puzzle(answer: 94)]
-    public long Part1Example() => new AClass().Part1(InputExample);
+    public long Part1Example() => new AClass(false).Part1(InputExample);
 
     //2274 too low
     [Puzzle(answer: 2442)]
-    public long Part1() => new AClass().Part1(Input);
+    public long Part1() => new AClass(false).Part1(Input);
 
     [Puzzle(answer: 154)]
-    public long Part2Example() => new AClass().Part1(InputExample, true);
+    public long Part2Example() => new AClass(true).Part1(InputExample);
 
     [Puzzle(answer: 6898)]
-    public long Part2() => new AClass().Part1(Input, true);
+    public long Part2() => new AClass(true).Part1(Input);
 
 
     private class AClass
     {
         private int[] XDir = [0, -1, 0, 1];
         private int[] YDir = [1, 0, -1, 0];
-        private bool _part2 = false;
+        private readonly bool Part2;
+        private List<(int, int)> Vertices = new();
+        private Dictionary<(int, int), Dictionary<(int, int), int>> Edges = new();
 
-        internal long Part1(string[] input, bool p2 = false)
+        public AClass(bool part2)
         {
-            _part2 = p2;
+            Part2 = part2;
+        }
+
+        internal long Part1(string[] input)
+        {
             input = input.Reverse().ToArray();
             var start = (1, input.Length - 1);
             var end = (input[0].Length - 2, 0);
 
             //reduce graph
-            var vertices = FindCrosspoints(input);
-            vertices.Insert(0, start);
-            vertices.Add(end);
+            Vertices = FindVertices(input);
+            Vertices.Insert(0, start);
+            Vertices.Add(end);
 
             //find all edges in graph
-            var connections = new Dictionary<(int, int), Dictionary<(int, int), int>>();
-            var enqueued = new HashSet<(int, int)>();
-            var queue = new Queue<(int, int)>();
-            queue.Enqueue(start);
-            foreach(var vertex in vertices)
+            foreach (var vertex in Vertices)
             {
                 var dict = new Dictionary<(int, int), int>();
-                Nexts(vertex, (0, 0), 0, vertices, input, dict);
-                connections[vertex] = dict;
+                Nexts(vertex, (0, 0), 0, input, dict);
+                Edges[vertex] = dict;
             }
 
             //find all paths
             var distances = new List<int>();
-            Paths(start, 0, 1L, end, connections, distances);
-            return distances.Max();
+            Paths(start, 0, 1L, distances, end);
+            return distances.Count;
         }
 
         void Paths(
             (int, int) current,
             int distance,
             long visited,
-            (int, int) to,
-            Dictionary<(int, int), Dictionary<(int, int), int>> connections,
-            List<int> distances)
+            List<int> distances,
+            (int, int) to)
         {
             if (current == to) distances.Add(distance);
-            var list = connections.Select(x => x.Key).ToList();
-            foreach (var next in connections[current])
+            foreach (var next in Edges[current])
             {
-                var index = list.IndexOf(next.Key);
+                var index = Vertices.IndexOf(next.Key);
                 if (((visited >> index) & 1) == 0)
                 {
-                    Paths(next.Key, distance + next.Value, visited + (1L << index), to, connections, distances);
+                    Paths(next.Key, distance + next.Value, visited + (1L << index), distances, to);
                 }
             }
         }
-/*
-        Dictionary<(int, int), int> Nexts2(
-            (int, int) current,
-            (int, int) lastDir,
-            List<(int, int)> crosspoints,
-            string[] input)
-        {
-            var dict = new Dictionary<(int, int), int>();
-            var queue = new Queue<((int, int), (int, int), int)>();
-            queue.Enqueue((current, lastDir, 0));
-            while (queue.TryDequeue(out var result))
-            {
-                var point = result.Item1;
-                lastDir = result.Item2;
-                var distance = result.Item3;
-                if (crosspoints.Contains(point) && distance > 0) { 
-                    dict.Add(point, distance);
-                    continue;
-                }
-                for (int k = 0; k < 4; k++)
-                {
-                    var dir = (XDir[k], YDir[k]);
-                    var next = (point.Item1 + dir.Item1, point.Item2 + dir.Item2);
-                    if (next.Item1 >= 0 && next.Item1 <= input[0].Length - 1
-                        && next.Item2 >= 0 && next.Item2 <= input.Length - 1
-                        && lastDir != (-dir.Item1, -dir.Item2)
-                        && input[next.Item2][next.Item1] != '#' && (Allowed(dir, input[next.Item2][next.Item1]) || _part2))
-                    {
-                        queue.Enqueue((next, dir, distance + 1));
-                    }
-                }
-            }
-            return dict;
-        }*/
 
-        void Nexts((int, int) current, (int, int) lastDir, int distance,
-            List<(int, int)> crosspoints, string[] input, Dictionary<(int, int), int> toDistances)
+        void Nexts((int, int) current, (int, int) lastDir, int distance, string[] input, Dictionary<(int, int), int> toDistances)
         {
-            if (distance > 0 && crosspoints.Contains(current))
+            if (distance > 0 && Vertices.Contains(current))
             {
                 toDistances.Add(current, distance);
                 return;
@@ -136,9 +102,9 @@ public sealed class Day23 : Day
                 if (next.Item1 >= 0 && next.Item1 <= input[0].Length - 1
                  && next.Item2 >= 0 && next.Item2 <= input.Length - 1
                  && lastDir != (-dir.Item1, -dir.Item2)
-                 && input[next.Item2][next.Item1] != '#' && (Allowed(dir, input[next.Item2][next.Item1]) || _part2))
+                 && input[next.Item2][next.Item1] != '#' && (Allowed(dir, input[next.Item2][next.Item1]) || Part2))
                 {
-                    Nexts(next, dir, distance + 1, crosspoints, input, toDistances);
+                    Nexts(next, dir, distance + 1, input, toDistances);
                 }
             }
         }
@@ -156,7 +122,7 @@ public sealed class Day23 : Day
             };
         }
 
-        private List<(int, int)> FindCrosspoints(string[] input)
+        private List<(int, int)> FindVertices(string[] input)
         {
             var kruispunten = new List<(int, int)>();
             for (int i = 1; i < input.Length - 1; i++)
