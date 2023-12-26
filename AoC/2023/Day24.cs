12815ceffe358153ffc704191da1ac63c848b655
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-26 01:12:25 +0100

    feat: 2023-24 gradient descent attempt

--- AoC/2023/Day24.cs
+++ AoC/2023/Day24.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
 using System.Linq;
 using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
@@ -81,12 +83,85 @@ public sealed class Day24 : Day
                     (double.Parse(split[3]), double.Parse(split[4]), double.Parse(split[5]))
                     );
             }).ToList();
+            var sorted = lines
+                .Select(x => new Line2D(new Point2D(x.P.X, x.P.Y), new Point2D(x.D.X, x.D.Y)))
+                .Select(x => $"y = {x.A}x + {x.B}")
+                .ToList();
 
-            foreach(var line in lines.Skip(1))
+            var f = (double t, double t2) =>
             {
+                var baseLine = new Line2D(new Point2D(lines[0].P.X, lines[0].P.Y), new Point2D(lines[0].D.X, lines[0].D.Y));
+                var targetLine = new Line2D(new Point2D(lines[1].P.X, lines[1].P.Y), new Point2D(lines[1].D.X, lines[1].D.Y));
+                var basePoint = baseLine.At(t);
+                var targetPoint = targetLine.At(t + t2);
+                var guessLine = Line2D.FromPoints(basePoint, targetPoint, t2);
 
+                //assert
+                var confirmTime = guessLine.IntersectionTime(targetLine, targetPoint);
+                //confirmTime.Should().BeApproximately(t2, 0.001);
+                return lines.Skip(2).Sum(line =>
+                {
+                    var currentLine = new Line2D(new Point2D(line.P.X, line.P.Y), new Point2D(line.D.X, line.D.Y));
+                    var intersection = guessLine.IntersectS(currentLine);
+                    if (intersection is null) return 10000;
+                    var time = currentLine.IntersectionTime(guessLine, intersection.Value)!.Value;
+                    var baseToIntersectionTime = guessLine.IntersectionTime(currentLine, intersection.Value)!.Value;
+                    var error = Math.Min(
+                        Math.Abs((t + baseToIntersectionTime - time)),
+                        Math.Abs((t - baseToIntersectionTime - time)));
+                    return error;
+                });
+            };
+            /*Console.WriteLine($"4.999: {f(4.9, 1)}");
+            Console.WriteLine($"5.000: {f(5.0, 1)}");
+            Console.WriteLine($"5.001: {f(5.1, 1)}");*/
+            /*for (int i = 0; i < 40; i++)
+            {
+                var time = 5 - 0.2 + (double)i / 100;
+                Console.WriteLine($"{time}: {f(time, -3, 1)}");
+            }*/
+
+            for (long i = 0; i <= 5_000_000_000_000; i += 100_000_000_000)
+            {
+                for (long j = -5_000_000_000_000; j <= 5_000_000_000_000; j += 400_000_000_000)
+                {
+                    //Console.WriteLine($"{4 + 0.04 * i}, {-3 + 0.04 * j}");
+                    //181274863478376
+                    var value = Math.Log(f(i, j));
+                    if (value < 0.1) Console.BackgroundColor = ConsoleColor.Green;
+                    else if (value < 0.2) Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    else if (value < 0.5) Console.BackgroundColor = ConsoleColor.Yellow;
+                    else if (value < 0.75) Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    else if(value < 1) Console.BackgroundColor = ConsoleColor.Red;
+                    else if(value < 2) Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Write(value.ToString("0.00 "));
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
             }
+            Console.WriteLine();
+
+            /*var learnDecay = 0.1d;
+            var learnRate = 1d;
+            var dp = new double[] { 0.0001, 0.0001 }; //derivative precision
+            var guess = (5d, -2d);
+
+            for (int i = 1; i < 100000; i++)
+            {
+                var currentError = f(guess.Item1, guess.Item2);
+                learnRate = Math.Exp(-1+-i/1000d);
+                var ft = (f(guess.Item1 + dp[0], guess.Item2) - f(guess.Item1 - dp[0], guess.Item2)) / (2 * dp[0]);
+                var ft2 = (f(guess.Item1, guess.Item2 + dp[1]) - f(guess.Item1, guess.Item2 - dp[1])) / (2 * dp[1]);
+                guess = (guess.Item1 - learnRate * ft, guess.Item2 - learnRate * ft2);
+                //guess = (guess.Item1, guess.Item2, guess.Item3 - learnRate * fdy);
+                if (i % 1000 == 0)
+                {
+                    var q = 1;
+                }
+            }*/
+            var a = 1;
             return 0;
+            //return (long)(guess.Item1 + guess.Item2);
         }
     }
 
@@ -134,9 +209,16 @@ public sealed class Day24 : Day
         public double A => Dir.Y / Dir.X;
         public double B => Point.Y - A * Point.X;
 
+        public static Line2D FromPoints(Point2D from, Point2D to, double time = 1)
+        {
+            return new Line2D(from, (to - from) / time);
+        }
+
         public Point2D? IntersectS(Line2D line)
         {
-            return new Point2D((line.B - B) / (A - line.A), A * (line.B - B) / (A - line.A) + B);
+            return (A - line.A) != 0
+                ? new Point2D((line.B - B) / (A - line.A), A * (line.B - B) / (A - line.A) + B)
+                : null;
         }
 
         public Point2D? Intersect(Line2D line)
@@ -160,6 +242,8 @@ public sealed class Day24 : Day
             var yNumerator = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4));
             return new Point2D((double)xNumerator / (double)denom, (double)yNumerator / (double)denom);
         }
+
+        public Point2D At(double time) => Point + time * Dir;
 
         public bool SameLine(Line2D line)
         {
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,4 +1,4 @@
 using AoC_2023;
 
-var result = new Day24().Part2Example();
+var result = new Day24().Part2();
 Console.WriteLine(result);
