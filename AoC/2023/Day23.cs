4dcd4ff642dd83e3be7f00de5dae03712cba78bf
Renzo Baasdam <renzobaasdam@gmail.com>
2023-12-31 22:54:26 +0100

    refactor: cleanup 2023-23

--- AoC/2023/Day23.cs
+++ AoC/2023/Day23.cs
@@ -5,16 +5,16 @@ public sealed class Day23 : Day
     private Algorithm Algo = Algorithm.DFS;
 
     [Puzzle(answer: 94)]
-    public long Part1Example() => new AClass(false, Algo).Part1(InputExample);
+    public long Part1Example() => new AClass(Algo).Part1(InputExample, false);
 
     [Puzzle(answer: 2442)]
-    public long Part1() => new AClass(false, Algo).Part1(Input);
+    public long Part1() => new AClass(Algo).Part1(Input, false);
 
     [Puzzle(answer: 154)]
-    public long Part2Example() => new AClass(true, Algo).Part1(InputExample);
+    public long Part2Example() => new AClass(Algo).Part1(InputExample, true);
 
     [Puzzle(answer: 6898)]
-    public long Part2() => new AClass(true, Algo).Part1(Input);
+    public long Part2() => new AClass(Algo).Part1(Input, true);
 
     private enum Algorithm
     {
@@ -27,47 +27,44 @@ public sealed class Day23 : Day
         private int[] XDir = [0, -1, 0, 1];
         private int[] YDir = [1, 0, -1, 0];
         private readonly Algorithm Algorithm;
-        private readonly bool Part2;
-        private List<(int, int)> Vertices = new();
+        private bool Part2;
+        private Dictionary<(int, int), int> Vertices = new();
         private Dictionary<(int, int), Dictionary<(int, int), int>> Edges = new();
         private (int, int) End = new();
 
-        public AClass(bool part2, Algorithm algorithm)
+        public AClass(Algorithm algorithm)
         {
-            Part2 = part2;
             Algorithm = algorithm;
         }
 
-        internal long Part1(string[] input)
+        internal long Part1(string[] input, bool part2)
         {
+            Part2 = part2;
             input = input.Reverse().ToArray();
             var start = (1, input.Length - 1);
             End = (input[0].Length - 2, 0);
 
             //reduce graph
             Vertices = FindVertices(input);
-            Vertices.Insert(0, start);
-            Vertices.Add(End);
+            Vertices.Add(start,0);
+            Vertices.Add(End, Vertices.Count);
 
             //find all edges in graph
-            foreach (var vertex in Vertices)
+            foreach (var vertex in Vertices.Keys)
             {
                 var dict = new Dictionary<(int, int), int>();
                 Nexts(vertex, (0, 0), 0, input, dict);
                 Edges[vertex] = dict;
             }
-
-            //find all paths
             return Algorithm == Algorithm.DFS
                 ? DfsPaths(start, 0, 1L)
                 : BfsPaths(start);
         }
 
-        int BfsPaths(
-            (int, int) current)
+        int BfsPaths((int, int) current)
         {
             int max = 0;
-            var dict = new Dictionary<((int, int), long), int>();
+            var dict = new Dictionary<((int, int), long), int>(3_000_000);
             var currentIteration = new Dictionary<((int, int), long), int>()
             {
                 { (current, 1L), 0 }
@@ -75,13 +72,13 @@ public sealed class Day23 : Day
             int count = 0;
             while (currentIteration.Count > 0)
             {
-                var nextIteration = new Dictionary<((int, int), long), int>();
+                var nextIteration = new Dictionary<((int, int), long), int>(3_000_000);
                 foreach (var state in currentIteration.Keys)
                 {
                     //all next vertices for current state
                     foreach (var next in Edges[state.Item1])
                     {
-                        var index = Vertices.IndexOf(next.Key);
+                        var index = Vertices[next.Key];
                         if (((state.Item2 >> index) & 1) == 0)
                         {
                             var nextState = (next.Key, state.Item2 + (1L << index));
@@ -114,7 +111,7 @@ public sealed class Day23 : Day
             int max = 0;
             foreach (var next in Edges[current])
             {
-                var index = Vertices.IndexOf(next.Key);
+                var index = Vertices[next.Key];
                 if (((visited >> index) & 1) == 0)
                 {
                     max = Math.Max(max, DfsPaths(next.Key, distance + next.Value, visited + (1L << index)));
@@ -125,7 +122,7 @@ public sealed class Day23 : Day
 
         void Nexts((int, int) current, (int, int) lastDir, int distance, string[] input, Dictionary<(int, int), int> toDistances)
         {
-            if (distance > 0 && Vertices.Contains(current))
+            if (distance > 0 && Vertices.ContainsKey(current))
             {
                 toDistances.Add(current, distance);
                 return;
@@ -158,9 +155,10 @@ public sealed class Day23 : Day
             };
         }
 
-        private List<(int, int)> FindVertices(string[] input)
+        private Dictionary<(int, int), int> FindVertices(string[] input)
         {
-            var kruispunten = new List<(int, int)>();
+            var kruispunten = new Dictionary<(int, int), int>();
+            int index = 1;
             for (int i = 1; i < input.Length - 1; i++)
             {
                 for (int j = 1; j < input[0].Length - 1; j++)
@@ -168,7 +166,7 @@ public sealed class Day23 : Day
                     if (input[i][j] == '.')
                     {
                         int sum = Enumerable.Range(0, 4).Count(k => input[i + YDir[k]][j + XDir[k]] != '#');
-                        if (sum > 2) kruispunten.Add((j, i));
+                        if (sum > 2) kruispunten.Add((j, i), index++);
                     }
                 }
             }
