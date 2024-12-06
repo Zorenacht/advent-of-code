c0047fe9eb5dce7c46cd8da8e802888bc83b0615
Renzo Baasdam <renzobaasdam@gmail.com>
2024-12-06 18:36:30 +0100

    refactor: 2024-06

--- src/AoC/2024/Day06.cs
+++ src/AoC/2024/Day06.cs
@@ -1,8 +1,3 @@
-using FluentAssertions;
-using System.Collections;
-using System.ComponentModel.Design;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices.JavaScript;
 using Tools.Geometry;
 
 namespace AoC_2024;
@@ -12,30 +7,6 @@ public sealed class Day06 : Day
     [Puzzle(answer: 4559)]
     public int Part1() => Path(Input).GroupBy(x => x.Index).Count();
     
-    private HashSet<IndexDir> Path(string[] lines)
-    {
-        var grid = lines.ToGrid();
-        var start = grid.FindIndexes('^').First();
-        var dir = Direction.N;
-        var set = new HashSet<IndexDir>();
-        while (true)
-        {
-            if (grid.ValueOrDefault(start) == null) break;
-            set.Add(new IndexDir(start, dir));
-            
-            var attempt = start.Neighbor(dir);
-            while (grid.ValueOrDefault(attempt) == '#')
-            {
-                dir = dir.Right();
-                attempt = start.Neighbor(dir);
-            }
-            start = attempt;
-        }
-        return set;
-    }
-    
-    
-    //not 551 or 550 or 3965 or 3964 or 1658 or 1646 or 1656 or 1623
     [Puzzle(answer: 1604)]
     public int Part2() => Loop(Input);
     
@@ -44,22 +15,37 @@ public sealed class Day06 : Day
     
     private record IndexDir(Index2D Index, Direction Direction);
     
+    private HashSet<IndexDir> Path(string[] lines)
+    {
+        var grid = lines.ToGrid();
+        var current = grid.FindIndexes('^').First();
+        var dir = Direction.N;
+        var set = new HashSet<IndexDir>();
+        while (true)
+        {
+            if (grid.ValueOrDefault(current) == null) break;
+            set.Add(new IndexDir(current, dir));
+            
+            var next = current + dir;
+            while (grid.ValueOrDefault(next) == '#')
+            {
+                dir = dir.Right();
+                next = current + dir;
+            }
+            current = next;
+        }
+        return set;
+    }
+    
     private int Loop(string[] lines)
     {
         var grid = lines.ToGrid();
-        var start = grid.FindIndexes('^').FirstOrDefault();
-        var dir = Direction.N;
-        int count = 0;
         var path = Path(lines);
-        foreach (var pair in path.Zip(path.Skip(1))
-                     .Where(pair => grid.ValueOrDefault(pair.Second.Index) is '.')
-                     .GroupBy(pair => pair.Second.Index)
-                     .Select(x => x.First()))
-        {
-            if (HasCycle(pair.First, pair.Second.Index, grid))
-                count++;
-        }
-        return count;
+        return path.Zip(path.Skip(1))
+            .Where(pair => grid.ValueOrDefault(pair.Second.Index) is '.')
+            .GroupBy(pair => pair.Second.Index)
+            .Select(x => x.First())
+            .Count(pair => HasCycle(pair.First, pair.Second.Index, grid));
     }
     
     private static bool HasCycle(IndexDir indexDir, Index2D blockCandidate, Grid<char> grid)
@@ -75,11 +61,11 @@ public sealed class Day06 : Day
                 return true;
             simulated.Add(new IndexDir(start, dir));
             
-            var next = start.Neighbor(dir);
+            var next = start + dir;
             while (grid.ValueOrDefault(next) == '#' || next == blockCandidate)
             {
                 dir = dir.Right();
-                next = start.Neighbor(dir);
+                next = start + dir;
             }
             start = next;
         }
--- src/Tools/Geometry/Index2D.cs
+++ src/Tools/Geometry/Index2D.cs
@@ -42,10 +42,17 @@ public readonly struct Index2D(int row, int col) : IEquatable<Index2D>
     public override bool Equals([NotNullWhen(true)] object? obj) => obj is Point other && Equals(other);
     public bool Equals(Index2D other) => other.Row == Row && other.Col == Col;
     public override int GetHashCode() => (Row + Col) * (Row + Col + 1) / 2 + Col;
+    
     public static bool operator ==(Index2D left, Index2D right) => left.Equals(right);
     public static bool operator !=(Index2D left, Index2D right) => !(left == right);
-    public static Index2D operator -(Index2D left, Index2D right) => new(left.Row - right.Row, left.Col - right.Col);
+    public static Index2D operator -(Index2D self) => new(-self.Row, -self.Col); // Does this make sense to exist?
+    public static Index2D operator +(Index2D self) => self; // Does this make sense to exist?
     public static Index2D operator +(Index2D left, Index2D right) => new(left.Row + right.Row, left.Col + right.Col);
+    public static Index2D operator -(Index2D left, Index2D right) => new(left.Row - right.Row, left.Col - right.Col);
+    public static Index2D operator +(Direction left, Index2D right) => right.Neighbor(left);
+    public static Index2D operator -(Direction left, Index2D right) => -right.Neighbor(left);
+    public static Index2D operator +(Index2D left, Direction right) => left.Neighbor(right);
+    public static Index2D operator -(Index2D left, Direction right) => left.Neighbor(right.Backwards());
     public static Index2D operator *(int left, Index2D right) => new(left * right.Row, left * right.Row);
     public static Index2D operator *(Index2D left, int right) => new(left.Row * right, left.Row * right);
 }
