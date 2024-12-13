c9871f1cf971a8a46bb4c63795fc01dbff73a000
Renzo Baasdam <renzobaasdam@gmail.com>
2024-12-13 07:30:50 +0100

    feat: 2024-13

---
+++ src/AoC/2024/Day13.cs
@@ -0,0 +1,54 @@
+using FluentAssertions;
+using System.Text.RegularExpressions;
+
+namespace AoC_2024;
+
+public sealed class Day13 : Day
+{
+    [Puzzle(answer: 35255)]
+    public long Part1() => MinCost(extraCost: 0);
+    
+    [Puzzle(answer: 87582154060429)]
+    public long Part2() => MinCost(extraCost: 10000000000000);
+    
+    private long MinCost(long extraCost)
+    {
+        long result = 0;
+        var groups = Input.GroupBy(string.Empty);
+        foreach (var group in groups)
+        {
+            var buttonRegex = new Regex(@"Button .: X\+(\d+), Y\+(\d+)");
+            var prizeRegex = new Regex(@"Prize: X=(\d+), Y=(\d+)");
+            var aMatches = buttonRegex.Match(group[0]);
+            var bMatches = buttonRegex.Match(group[1]);
+            var prizeMatches = prizeRegex.Match(group[2]);
+            int[] bA = [int.Parse(aMatches.Groups[1].Value), int.Parse(aMatches.Groups[2].Value)];
+            int[] bB = [int.Parse(bMatches.Groups[1].Value), int.Parse(bMatches.Groups[2].Value)];
+            long[] prize = [extraCost + int.Parse(prizeMatches.Groups[1].Value), extraCost + int.Parse(prizeMatches.Groups[2].Value)];
+            
+            var matrix = new int[][]
+            {
+                [bA[0], bB[0]],
+                [bA[1], bB[1]]
+            };
+            var inverse = new int[][]
+            {
+                [+matrix[1][1], -matrix[0][1]],
+                [-matrix[1][0], +matrix[0][0]]
+            };
+            var inverseFactor = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+            var aPresses = prize[0] * inverse[0][0] + prize[1] * inverse[0][1];
+            var bPresses = prize[0] * inverse[1][0] + prize[1] * inverse[1][1];
+            
+            if (inverseFactor == 0) throw new NotSupportedException("No unique solution if not invertible.");
+            if (aPresses % inverseFactor != 0 || bPresses % inverseFactor != 0) continue;
+            aPresses /= inverseFactor;
+            bPresses /= inverseFactor;
+            
+            result += aPresses < 0 || bPresses < 0
+                ? 0
+                : aPresses * 3 + bPresses;
+        }
+        return result;
+    }
+};
