79e943ceb6660548f09b3c184200fd537e795a7f
Renzo Baasdam <renzobaasdam@gmail.com>
2024-12-18 10:45:22 +0100

    perf: 2024-18 binary search

--- src/AoC/2024/Day18.cs
+++ src/AoC/2024/Day18.cs
@@ -7,37 +7,42 @@ namespace AoC._2024;
 [SuppressMessage("ReSharper", "InlineTemporaryVariable")]
 public sealed class Day18 : Day
 {
-    
     [Puzzle(answer: 22)]
     public long Part1_Example()
     {
-        var maze = InitMaze(7,7, 12, InputExample);
+        var maze = InitMaze(7, 7, 12, InputExample);
         return ShortestPath(Index2D.O, new Index2D(maze.RowLength - 1, maze.ColLength - 1), maze);
     }
     
     [Puzzle(answer: 336)]
     public long Part1()
     {
-        var maze = InitMaze(71,71, 1024, Input);
+        var maze = InitMaze(71, 71, 1024, Input);
         return ShortestPath(Index2D.O, new Index2D(maze.RowLength - 1, maze.ColLength - 1), maze);
     }
     
+    //2951
     [Puzzle(answer: "24,30")]
     public string Part2()
     {
+        const int size = 71;
         var lines = Input;
-        var maze = InitMaze(71,71, 1024, lines);
-        for (int i = 1024; i < lines.Length; ++i)
+        var start = Index2D.O;
+        var end = new Index2D(size - 1, size - 1);
+        var left = 0;
+        var right = lines.Length - 1;
+        while (left < right)
         {
-            var ints = lines[i].Ints();
-            maze.UpdateAt(ints[0], ints[1], '#');
-            if (ShortestPath(Index2D.O, new Index2D(maze.RowLength - 1, maze.ColLength - 1), maze) != -1) continue;
-            return lines[i];
+            var mid = (left + right + 1) / 2;
+            var midMaze = InitMaze(size, size, mid + 1, lines);
+            var midValue = ShortestPath(start, end, midMaze);
+            if (midValue == -1) right = mid;
+            else left = mid + 1;
         }
-        return string.Empty;
+        return lines[left];
     }
     
-    public Maze InitMaze(int rows, int cols, int take,  string[] blockades)
+    private static Maze InitMaze(int rows, int cols, int take, string[] blockades)
     {
         var maze = new Maze(rows, cols);
         foreach (var line in blockades.Take(take))
@@ -53,7 +58,6 @@ public sealed class Day18 : Day
         var pq = new PriorityQueue<DijkstraNode, int>();
         pq.Enqueue(new DijkstraNode(start, null), 0);
         var nodes = new Dictionary<Index2D, DijkstraInfo>();
-        maze.Print();
         while (pq.TryDequeue(out var dnode, out var distance))
         {
             var currentIndex = dnode.Current;
@@ -82,7 +86,7 @@ public sealed class Day18 : Day
             
             foreach (var nb in Directions.CardinalIndex.Select(dir => currentIndex + dir)
                          .Where(nb => maze.ValueOrDefault(nb) == '.'))
-                         //.Where(nb => !nodes.ContainsKey(nb)))
+                //.Where(nb => !nodes.ContainsKey(nb)))
             {
                 pq.Enqueue(new DijkstraNode(nb, currentIndex), distance + 1);
             }
